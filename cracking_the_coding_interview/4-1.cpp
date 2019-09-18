36c6a3d7ca27c81a1b561e87e2b365acfef817e1
Mei Takahashi <me.7tspr@gmail.com>
2019-09-18 18:21:41 +0900

    Improve time complexity with using set.

--- cracking_the_coding_interview/4-1.cpp
+++ cracking_the_coding_interview/4-1.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <stack>
 #include <vector>
+#include <set>
 using namespace std;
+#define WHITE 0
 
 stack<int> s;
 
@@ -10,12 +12,17 @@ vector<vector<int> > NodeList = {{1, 2}, {0, 2}, {}};
 bool isPath(int start, int last) {
     if (start == last) return true;
     s.push(start);
+    set<int> visited = { start };
     while(!s.empty()) {
         int node = s.top();
         s.pop();
         for (int nextNode : NodeList[node]) {
             if(nextNode == last) return true;
-            s.push(nextNode);
+            auto search = visited.find(nextNode);
+            if(search == visited.end()) {
+                s.push(nextNode);
+                visited.insert(nextNode);
+            }
         }
     }
     return false;
