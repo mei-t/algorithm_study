bce8ccc38b994f83ce5569f2f13892e946b2876f
Mei Takahashi <me.7tspr@gmail.com>
2019-11-14 21:14:19 +0900

    Fix 2-3.cpp

--- cracking_the_coding_interview/2-3.cpp
+++ cracking_the_coding_interview/2-3.cpp
@@ -1,19 +1,36 @@
 #include <iostream>
 using namespace std;
 
-// 3分16秒
+// 3分16秒 +α
 
 struct Node{
     int val;
     Node* next;
 };
 
-void deteleMiddleNode(Node* node){
-    Node* delNode = node;
-    node = node -> next;
+// void deleteMiddleNode(Node* node){
+//     Node* delNode = node;
+//     node = node -> next;
+//     delete delNode;
+// }
+
+void deleteMiddleNode(Node* node){
+    Node* delNode = node->next;
+    *node = *node->next;
     delete delNode;
 }
 
 int main(void){
+    Node* node0 = new Node;
+    node0 -> val = 0;
+    Node* node1 = new Node;
+    node0 -> next = node1;
+    node1 -> val = 1;
+    Node* node2 = new Node;
+    node1 -> next =  node2;
+    node2 -> val = 2;
+    cout << node0 -> next -> val << endl;
+    deleteMiddleNode(node1);
+    cout << node0 -> next -> val << endl;
     return 0;
 }
