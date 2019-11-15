232d0600557cc4a899a099f2968aa3551dc8677b
Mei Takahashi <me.7tspr@gmail.com>
2019-11-15 16:35:26 +0900

    Solve advanced problem

--- cracking_the_coding_interview/8-3.cpp
+++ cracking_the_coding_interview/8-3.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-// 発展問題はまだ解いてない
+// 発展問題
+int findMagicIndex(const vector<int>& A, int left, int right){
+    if(left > right){
+        return -1;
+    }
+
+    int i = (left + right) / 2;
+
+    if(A[i] == i){
+        return i;
+    }
+    
+    // 左側
+    int newRight = min(i - 1, A[i]);
+    int leftResult = findMagicIndex(A, left, newRight);
+    if(leftResult >= 0){
+        return leftResult;
+    }
+
+    // 右側
+    int newLeft = max(i + 1, A[i]);
+    return findMagicIndex(A, newLeft, right);
+}
 
 int findMagicIndex(const vector<int>& A){
+    return findMagicIndex(A, 0, A.size() - 1);
+}
+
+int findMagicIndexWithoutDuplicate(const vector<int>& A){
     int left = 0;
     int right = A.size();
     int i;
@@ -23,6 +50,9 @@ int findMagicIndex(const vector<int>& A){
 
 int main(void){
     vector<int> A = {-3, -1, 0, 3, 9};
-    cout << findMagicIndex(A) << endl;
+    cout << findMagicIndex(A) << endl; // 3
+
+    vector<int> B = {-3, 3, 3, 3, 3};
+    cout << findMagicIndex(B) << endl; // 3
     return 0;
 }
