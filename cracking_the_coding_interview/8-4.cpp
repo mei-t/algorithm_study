62a77621dd9a39068fbd91fb7f4c3d577d31baf4
Mei Takahashi <me.7tspr@gmail.com>
2019-11-17 17:53:20 +0900

    Add 8-4.cpp

---
+++ cracking_the_coding_interview/8-4.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// 11分4秒
+
+template <typename T>
+// vectorのpush_back(value)はcopy of valueをvectorに入れるからsubSetは参照で良さそうだと思ったけど、だめだった
+void createSubSet(vector<vector<T>>& ans, const vector<T>& allSet, vector<T> subSet, int index){
+    if(index == allSet.size()){
+        return;
+    }
+
+    createSubSet(ans, allSet, subSet, index + 1);
+    subSet.push_back(allSet[index]);
+    ans.push_back(subSet);
+    createSubSet(ans, allSet, subSet, index + 1);
+    return;
+}
+
+template <typename T>
+vector<vector<T>> allSubSet(const vector<T>& allSet){
+    vector<vector<T>> ans;
+    vector<T> subSet;
+    ans.push_back(subSet);
+    createSubSet(ans, allSet, subSet, 0);
+    return ans;
+}
+
+
+int main(void){
+    vector<int> input = {0, 1, 2};
+    vector<vector<int>> output = allSubSet(input);
+    for(vector<int> subset : output){
+        for (int num : subset){
+            cout << num << ' ';
+        }
+        cout << endl;
+    }
+    return 0;
+}
