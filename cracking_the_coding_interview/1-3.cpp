69e9d66d56b0b6d1e26fc003d7092db89240849c
Mei Takahashi <me.7tspr@gmail.com>
2019-11-17 14:50:21 +0900

    Modify 1-3.cpp

--- cracking_the_coding_interview/1-3.cpp
+++ cracking_the_coding_interview/1-3.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
+#include <cassert>
 using namespace std;
 
-string urlify(const string& s){
-    string ans;
-    for(char c: s){
-        if(c == ' '){
-            ans += "%20";
-        }else{
-            ans += c;
+// 10分28
+string urlify(string s){
+    int i = 0;
+    while(i != s.size()){
+        if(s[i] == ' '){
+            s.replace(i, 1, "%20");
+            i += 2;
         }
+        i++;
     }
-    return ans;
+    return s;
 }
 
 int main(void){
+    cout << urlify("This is a pen.") << endl;
+    assert(urlify("This is a pen.") == "This%20is%20a%20pen.");
     return 0;
 }
