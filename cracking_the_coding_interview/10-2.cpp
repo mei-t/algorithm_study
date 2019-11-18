a136dd21cf19f9c0f0bd1c3832b1e005ad68d39d
Mei Takahashi <me.7tspr@gmail.com>
2019-11-18 18:40:44 +0900

    Fix 10-2.cpp

--- cracking_the_coding_interview/10-2.cpp
+++ cracking_the_coding_interview/10-2.cpp
@@ -2,49 +2,39 @@
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
+#include <algorithm>
 using namespace std;
-
-bool isAnaglam(string s1, string s2){
-    unordered_set<char> charInS1;
-    for(char c : s1){
-        charInS1.insert(c);
-    }
-    for(char c : s2){
-        if(charInS1.find(c) == charInS1.end()){
-            return false;
-        }
-    }
-    return true;
-}
 
 vector<string> anaglamGroup(const vector<string>& sList){
     vector<string> ans;
 
-    unordered_map<int, vector<string> > sMap;
+    unordered_map<int, vector<pair<string, string>> > sMap;
     vector<int> sizeList;
     // バケツソート
     // TC: O(sList.size())
     // SC: O(sList.size() * ave(s.size()))
     for(string s: sList) {
         if(sMap.find(s.size()) == sMap.end()){
-            sMap.insert({s.size(), vector<string>()});
+            sMap.insert({s.size(), vector<pair<string, string>>()});
             sizeList.push_back(s.size());
         }
-        sMap[s.size()].push_back(s);
+        string sorted = s;
+        sort(sorted.begin(), sorted.end());
+        sMap[s.size()].push_back(make_pair(s, sorted));
     }
 
     unordered_set<string> blackList;
     for(int size: sizeList){
-        const vector<string> &stringList = sMap[size];
+        const vector<pair<string, string>> &stringList = sMap[size];
         for(int i = 0; i < sMap[size].size(); i++){
-            const string &s1 = stringList[i];
-            if(blackList.find(s1) == blackList.end()) {
-                ans.push_back(s1);
+            const pair<string, string> &s1_pair = stringList[i];
+            if(blackList.find(s1_pair.first) == blackList.end()) {
+                ans.push_back(s1_pair.first);
                 for(int j = i + 1; j < stringList.size(); j++){
-                    const string s2 = stringList[j];
-                    if(blackList.find(s2) == blackList.end() && isAnaglam(s1, s2)) {
-                        ans.push_back(s2);
-                        blackList.insert(s2);
+                    const pair<string, string> s2_pair = stringList[j];
+                    if(blackList.find(s2_pair.first) == blackList.end() && s1_pair.second == s2_pair.second) {
+                        ans.push_back(s2_pair.first);
+                        blackList.insert(s2_pair.first);
                     }
                 }
             }
@@ -55,5 +45,11 @@ vector<string> anaglamGroup(const vector<string>& sList){
 }
 
 int main(void){
+    vector<string> v = {"aac", "kdnsr", "dkx", "cac", "aca"};
+    vector<string> ans = anaglamGroup(v);
+    for(string s: ans){
+        cout << s << " ";
+    }
+    cout << endl;
     return 0;
 }
