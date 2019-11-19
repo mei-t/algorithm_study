ae52ef06c17b875b6bb238459ac8f4823d0c2a03
Mei Takahashi <me.7tspr@gmail.com>
2019-11-19 19:54:27 +0900

    Fix 2-5.cpp

--- cracking_the_coding_interview/2-5.cpp
+++ cracking_the_coding_interview/2-5.cpp
@@ -8,68 +8,35 @@ struct Node{
     Node* next;
 };
 
-Node* sum(Node* node1, Node* node2){
-    bool isFinished1 = !node1;
-    bool isFinished2 = !node2;
-    Node* ans;
-    Node* current = ans;
-    Node* prev;
-    int advance = 0;
-    while(!(isFinished1 && isFinished2)){
-        int num1 = 0;
-        int num2 = 0;
-        if(!isFinished1){
-            num1 = node1->val;
-            if(node1->next){
-                node1 = node1->next;
-            }else{
-                isFinished1 = true;
-            }
-        }
-        if(!isFinished2){
-            num2 = node2->val;
-            if(node2->next){
-                node2 = node2->next;
-            }else{
-                isFinished2 = true;
-            }
-        }
+Node* sumList(Node* node1, Node* node2, int advance){
+    if(!(node1 && node2)){
+        return nullptr;
+    }
 
-        int sum = num1 + num2 + advance;
-        advance = sum / 10;
-        sum %= 10;
-        current = new Node;
-        current->val = sum;
-        if(prev){
-            prev->next = current;
-        }
-        prev = current;
-    }
-    if(advance){
-        current = new Node;
-        current->val = advance;
-        prev->next = current; // advance != 0の時while文の中が1回は実行されたということ。この時prevはnullではない
-    }
-    return ans;
+    int num1 = node1 ? node1->val : 0;
+    int num2 = node2 ? node2->val : 0;
+    int sum = num1 + num2 + advance;
+    advance = sum / 10;
+    sum %= 10;
+    Node* current = new Node;
+    current->val = sum;
+    node1 = node1 ? node1->next : node1;
+    node2 = node2 ? node2->next : node2;
+    current->next = sumList(node1, node2, advance);
+    return current;
 }
 
-Node* createInput(const vector<int>& nums){
-    Node* first;
-    Node* current = first;
-    Node* prev;
-    for(int num: nums){
-        current = new Node;
-        current->val = num;
-        if(prev){
-            prev->next = current;
-        }
-        prev = current;
+Node* createNextNode(const vector<int>& nums, size_t pos){
+    if(pos>= nums.size()){
+        return nullptr;
     }
-    return first;
+    Node* current = new Node;
+    current->val = nums[pos];
+    current->next = createNextNode(nums, pos+1);
+    return current;
 }
 
 void print(Node* node){
-    cout << "print start" << endl;
     while(node){
         cout << node->val;
         if(node->next == nullptr){
@@ -83,13 +50,11 @@ void print(Node* node){
 int main(void){
     vector<int> nums1 = {7, 1, 6};
     vector<int> nums2 = {5, 9, 2};
-    Node* node1 = createInput(nums1);
-    Node* node2 = createInput(nums2);
-    // print(node1);
-    // print(node2);
-    cout << "createInput sucesses." << endl;
-    Node* ans = sum(node1, node2);
-    cout << "aaa" << endl;
-    // print(ans);
+    Node* node1 = createNextNode(nums1, 0);
+    Node* node2 = createNextNode(nums2, 0);
+    print(node1);
+    print(node2);
+    Node* ans = sumList(node1, node2, 0);
+    print(ans);
     return 0;
 }
