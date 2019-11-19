2cace346ef0b078bbe388e509b4088d6996c7af6
Mei Takahashi <me.7tspr@gmail.com>
2019-11-19 15:54:45 +0900

    Use another approach.

--- cracking_the_coding_interview/1-5.cpp
+++ cracking_the_coding_interview/1-5.cpp
@@ -1,40 +1,29 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 bool canChangeOnece(string s1, string s2){
-    string shortString, longString;
-    if(s1.size() <= s2.size()){
-        shortString = s1;
-        longString = s2;
-    }else{
-        shortString = s2;
-        longString = s1;
-    }
-    int diff = longString.size() - shortString.size();
-    if(diff == 0){
-        bool isDiff = false;
-        for(int i=0; i<shortString.size(); i++){
-            if(shortString[i] != longString[i]){
-                if(isDiff){
-                    return false;
-                }
-                isDiff = true;
-            }
-        }
-    }else if(diff == 1){
-        int shortIndex = 0;
-        int longIndex = 0;
-        while(shortIndex < shortString.size()){
-            if(shortString[shortIndex] != longString[longIndex]){
-                if(shortIndex != longIndex || shortString[shortIndex] != longString[++longIndex]){
-                    return false;
-                }
-            }
-            shortIndex++;
-            longIndex++;
-        }
-    }else{
+    if(abs((int)s1.size() - (int)s2.size()) > 1){
         return false;
+    }
+    string shortString = s1.size() < s2.size() ? s1 : s2;
+    string longString = s1.size() < s2.size() ? s2 : s1;
+    int index1 = 0;
+    int index2 = 0;
+    bool isDiff = false;
+    while(index1 < shortString.size() && index2 < longString.size()){
+        if(shortString[index1] != longString[index2]){
+            if(isDiff){
+                return false;
+            }
+            isDiff = true;
+            if(shortString.size() == longString.size()){
+                index1++;
+            }
+        }else{
+            index1++;
+        }
+        index2++;
     }
     return true;
 }
