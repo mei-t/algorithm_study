7d4288b55410b10f88a69feb91521d42ebbc86f8
Mei Takahashi <me.7tspr@gmail.com>
2019-11-23 12:51:47 +0900

    Add 1-6.cpp

---
+++ cracking_the_coding_interview/1-6.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <charconv>
+using namespace std;
+
+void stringCompression(char* s, const size_t size){
+    int count = 1;
+    int diff = 0;
+    vector<pair<char, int>> charList;
+    for(int i=1; i<size; i++){
+        if(s[i] != s[i-1]){
+            charList.push_back(make_pair(s[i-1], count));
+            diff += count - 2;
+            count = 1;
+        }else{
+            count++;
+        }
+    }
+
+    if(diff <= 0){
+        return;
+    }
+
+    int i = 0;
+    for(auto pair: charList){
+        s[i] = pair.first;
+        s[i+1] = pair.second;
+        i += 2;
+    }
+    s[size - diff] = '\0';
+    return;
+}
+
+int main(void){
+    char *s = (char*)"aaabbcccccd";
+    stringCompression(s, strlen(s) + 1);
+    cout << s << endl;
+    return 0;
+}
