424c611fe8490e12f634733b6acf7eb04a4d2a4e
Mei Takahashi <me.7tspr@gmail.com>
2019-11-24 18:21:33 +0900

    Modify 5-3.cpp

--- cracking_the_coding_interview/5-3.cpp
+++ cracking_the_coding_interview/5-3.cpp
@@ -3,33 +3,27 @@ using namespace std;
 
 int countContinuedOne(int input){
     int pos = 1;
-    int ansCount = 0;
+    unsigned int ansCount = 0;
     int prevCount = 0;
     int curCount = 0;
-    int tmpCount = 0;
     while(pos != 0){
-        if(input && pos){
+        if(input & pos){
             curCount++;
         }else{
-            if(curCount != 0){
-                if(prevCount == 0){
-                    tmpCount = curCount + 1;
-                }else{
-                    tmpCount = prevCount + curCount + 1;
-                }
-                if(tmpCount > ansCount){
-                    ansCount = tmpCount;
-                }
-                prevCount = curCount;
-                curCount = 0;
-            }else{
-                prevCount = 0;
+            int tmpCount = prevCount + curCount + 1;
+            if(tmpCount > ansCount){
+                ansCount = tmpCount;
             }
+            prevCount = curCount;
+            curCount = 0;
         }
-        pos << 1;
+        pos <<= 1;
     }
-
-    return ansCount;
+    int tmpCount = prevCount + curCount + 1;
+    if(tmpCount > ansCount){
+        ansCount = tmpCount;
+    }
+    return min(ansCount, sizeof(int)*8);
 }
 
 int main(void){
