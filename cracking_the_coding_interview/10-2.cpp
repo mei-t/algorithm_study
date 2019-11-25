bc68768e3049820f4a57f4b4dc3e05c0492cbe22
Mei Takahashi <me.7tspr@gmail.com>
2019-11-25 21:22:57 +0900

    Modify

--- cracking_the_coding_interview/10-2.cpp
+++ cracking_the_coding_interview/10-2.cpp
@@ -13,7 +13,7 @@ vector<string> anaglamGroup(const vector<string>& sList){
     // バケツソート
     // TC: O(sList.size())
     // SC: O(sList.size() * ave(s.size()))
-    for(string s: sList) {
+    for(const string& s: sList) {
         if(sMap.find(s.size()) == sMap.end()){
             sMap.insert({s.size(), vector<pair<string, string>>()});
             sizeList.push_back(s.size());
@@ -23,19 +23,37 @@ vector<string> anaglamGroup(const vector<string>& sList){
         sMap[s.size()].push_back(make_pair(s, sorted));
     }
 
-    unordered_set<string> blackList;
-    for(int size: sizeList){
-        const vector<pair<string, string>> &stringList = sMap[size];
-        for(int i = 0; i < sMap[size].size(); i++){
-            const pair<string, string> &s1_pair = stringList[i];
-            if(blackList.find(s1_pair.first) == blackList.end()) {
-                ans.push_back(s1_pair.first);
-                for(int j = i + 1; j < stringList.size(); j++){
-                    const pair<string, string> s2_pair = stringList[j];
-                    if(blackList.find(s2_pair.first) == blackList.end() && s1_pair.second == s2_pair.second) {
-                        ans.push_back(s2_pair.first);
-                        blackList.insert(s2_pair.first);
-                    }
+    // unordered_set<string> blackList;
+    // for(int size: sizeList){
+    //     const vector<pair<string, string>> &stringList = sMap[size];
+    //     for(int i = 0; i < stringList.size(); i++){
+    //         const pair<string, string> &s1_pair = stringList[i];
+    //         if(blackList.find(s1_pair.first) == blackList.end()) {
+    //             ans.push_back(s1_pair.first);
+    //             for(int j = i + 1; j < stringList.size(); j++){
+    //                 const pair<string, string> s2_pair = stringList[j];
+    //                 if(blackList.find(s2_pair.first) == blackList.end() && s1_pair.second == s2_pair.second) {
+    //                     ans.push_back(s2_pair.first);
+    //                     blackList.insert(s2_pair.first);
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+
+    // iterator version
+    for (auto itr = sMap.begin(); itr != sMap.end(); itr++){
+        auto &stringList = itr->second;
+        for(auto itr2 = stringList.begin(); itr2 != stringList.end(); itr2++){
+            auto &s1_pair = *itr2;
+            ans.push_back(s1_pair.first);
+            for(auto itr3 = itr2 + 1; itr3 != stringList.end();){
+                auto &s2_pair = *itr3;
+                if(s1_pair.second == s2_pair.second){
+                    ans.push_back(s2_pair.first);
+                    itr3 = stringList.erase(itr3);
+                }else{
+                    itr3++;
                 }
             }
         }
