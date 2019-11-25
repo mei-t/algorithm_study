24b47292215107d4a6078ccae8e38792f7c843f1
Mei Takahashi <me.7tspr@gmail.com>
2019-11-25 19:43:04 +0900

    Modify

--- cracking_the_coding_interview/2-6.cpp
+++ cracking_the_coding_interview/2-6.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 using namespace std;
 
+// 15分17秒
+
 struct Node{
     int val;
     Node* next;
@@ -21,6 +23,7 @@ bool isPalindrome(Node* root){
     checkList(root, numList);
     auto fromTop = numList.begin();
     auto fromEnd = numList.end();
+    fromEnd--;
     for(int i=0; i < numList.size() - 1 / 2; i++){
         if(*fromTop != *fromEnd){
             return false;
@@ -28,9 +31,25 @@ bool isPalindrome(Node* root){
         fromTop++;
         fromEnd--;
     }
-    return false;
+    return true;
+}
+
+Node* createNode(const vector<int>& v, int i){
+    if(i == v.size()){
+        return nullptr;
+    }
+    Node* node = new Node;
+    node->val = v[i];
+    node->next = createNode(v, i+1);
+    return node;
 }
 
 int main(void){
+    vector<int> v1 = {0, 1, 4, 2, 4, 1, 0};
+    Node* node1 = createNode(v1, 0);
+    cout << isPalindrome(node1) << endl;
+    vector<int> v2 = {0, 1, 4, 2, 9, 4, 1, 0};
+    Node* node2 = createNode(v2, 0);
+    cout << isPalindrome(node2) << endl;
     return 0;
 }
