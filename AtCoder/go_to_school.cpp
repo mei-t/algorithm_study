cba04d591650cf7b691d6313f07fba65d24df91e
Mei Takahashi <me.7tspr@gmail.com>
2019-11-27 16:07:46 +0900

    go_to_school.cpp

---
+++ AtCoder/go_to_school.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void){
+
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    for(int i=0; i<N; i++){
+        cin >> A[i];
+    }
+    vector<int> ans(N);
+    for (int i=0; i<N; i++){
+        ans[A[i] - 1] = i + 1;
+    }
+    for(int i=0; i<N; i++){
+        cout << ans[i];
+        if(i < N - 1){
+        cout << " ";
+        }
+    }
+    cout << endl;
+    return 0;
+}
