7931f064a63067fc98ec868cdf14e2998a4043b0
Mei Takahashi <me.7tspr@gmail.com>
2019-11-28 17:56:30 +0900

    Delete unused code.

--- cracking_the_coding_interview/16-6.cpp
+++ cracking_the_coding_interview/16-6.cpp
@@ -10,15 +10,9 @@ int findMinDiff(vector<int>& v1, vector<int>& v2){
 
     sort(v1.begin(), v1.end());
     sort(v2.begin(), v2.end());
-    // for(int num: v2){
-    //     cout << num << endl;
-    // }
 
     vector<int> checkAll = v1[v1.size()-1] < v2[v2.size()-1] ? v1 : v2; 
-    vector<int> checkDiff = v1[v1.size()-1] >= v2[v2.size()-1] ? v1 : v2; 
-    // for(int num: checkAll){
-    //     cout << num << endl;
-    // }
+    vector<int> checkDiff = v1[v1.size()-1] >= v2[v2.size()-1] ? v1 : v2;
 
     int minDiff = INT_MAX;
     int cdPos = 0;
