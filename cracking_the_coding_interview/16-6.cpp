13e4e9cf53ddf283c8358005b60dbc830a0c927b
Mei Takahashi <me.7tspr@gmail.com>
2019-11-28 17:54:30 +0900

    Add 16-6.cpp

---
+++ cracking_the_coding_interview/16-6.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int findMinDiff(vector<int>& v1, vector<int>& v2){
+    if(v1.empty() || v2.empty()){
+        return -1; // error
+    }
+
+    sort(v1.begin(), v1.end());
+    sort(v2.begin(), v2.end());
+    // for(int num: v2){
+    //     cout << num << endl;
+    // }
+
+    vector<int> checkAll = v1[v1.size()-1] < v2[v2.size()-1] ? v1 : v2; 
+    vector<int> checkDiff = v1[v1.size()-1] >= v2[v2.size()-1] ? v1 : v2; 
+    // for(int num: checkAll){
+    //     cout << num << endl;
+    // }
+
+    int minDiff = INT_MAX;
+    int cdPos = 0;
+    for (int num: checkAll){
+        int diff = INT_MAX;
+        for(; cdPos < checkDiff.size(); cdPos++){
+            cout << num - checkDiff[cdPos] << endl;
+            if(abs(diff) < abs(num - checkDiff[cdPos])){
+                break;
+            }
+            diff = num - checkDiff[cdPos];
+        }
+        cdPos--;
+        diff = abs(diff);
+        if(minDiff > diff){
+            minDiff = diff;
+        }
+    }
+    return minDiff;
+}
+
+int main(void){
+    vector<int> v1 = {1, 2, 3, 11, 15};
+    vector<int> v2 = {8, 19, 23, 127, 235};
+    cout << findMinDiff(v1, v2) << endl;
+    return 0;
+}
