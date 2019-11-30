7b3943cf848e863047c701d2e9220bddc16afa58
Mei Takahashi <me.7tspr@gmail.com>
2019-11-30 09:16:00 +0900

    Modify

--- cracking_the_coding_interview/17-2.cpp
+++ cracking_the_coding_interview/17-2.cpp
@@ -3,26 +3,43 @@
 #include <cstdlib>
 using namespace std;
 
-void shuffle(vector<int>& cards, size_t i){
+void shuffle1(vector<int>& cards, size_t i){
     if(i == cards.size()){
         return;
     }
 
     size_t j = rand()/((RAND_MAX + 1u) / (i + 1));
     swap(cards[i], cards[j]);
-    shuffle(cards, i+1);
+    shuffle1(cards, i+1);
+}
+
+void shuffle2(vector<int>& cards, size_t i){
+    if(i == cards.size()){
+        return;
+    }
+
+    srand(time(0));
+    size_t j = rand()/((RAND_MAX + 1u) / (i + 1));
+    swap(cards[i], cards[j]);
+    shuffle2(cards, i+1);
+}
+
+void print(const vector<int>& cards){
+    for(int num: cards){
+        cout << num << " ";
+    }
+    cout << endl;
 }
 
 void shuffle(vector<int>& cards){
-    shuffle(cards, 0);
+    shuffle1(cards, 0);
+    print(cards);
+    shuffle2(cards, 0);
+    print(cards);
 }
 
 int main(void){
     vector<int> cards = {7, 35, 1, 50, 24};
     shuffle(cards);
-    for(int num: cards){
-        cout << num << " ";
-    }
-    cout << endl;
     return 0;
 }
