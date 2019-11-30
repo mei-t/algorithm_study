b9b11c63161af1e16891d906a135cc6ade0fc1fa
Mei Takahashi <me.7tspr@gmail.com>
2019-11-30 14:50:10 +0900

    Modify word_break_2.cpp

--- LeetCode/word_break_2.cpp
+++ LeetCode/word_break_2.cpp
@@ -1,7 +1,49 @@
 #include <iostream>
 #include <vector>
+#include <unordered_set>
+#include <unordered_map>
 using namespace std;
 #define SIZE 26
+
+class Solution {
+public:
+    vector<string> wordBreak(string s, unordered_set<string>& wordDict) {
+        if(memo.count(s)){
+            return memo[s];
+        }
+        
+        vector<string> result;
+        if(wordDict.count(s)){
+            result.push_back(s);
+        }
+        
+        for(int i=0; i<s.size(); i++){
+            string word = s.substr(i);
+            if(wordDict.count(word)){
+                string res = s.substr(0, i);
+                vector<string> prev = combine(word, wordBreak(res, wordDict));
+                result.insert(result.end(), prev.begin(), prev.end());
+            }
+        }
+        memo.insert({s, result});
+        return result;
+    }
+    
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
+        return wordBreak(s, wordSet);
+    }
+    
+    vector<string> combine(string word, vector<string> prev){
+        for(string& s: prev){
+            s += ' ' + word;
+        }
+        return prev;
+    }
+
+private:
+    unordered_map<string, vector<string>> memo;
+};
 
 // trieを使って解く問題かと思ったけど、メモリを使いすぎたみたい。なんで？
 // trieのspace complexityはどれくらい？
