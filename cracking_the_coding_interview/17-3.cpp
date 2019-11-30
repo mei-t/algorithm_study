7953562889c1be61e00e7236c7848f53e0c60c34
Mei Takahashi <me.7tspr@gmail.com>
2019-11-30 15:06:01 +0900

    Fix 17-3.cpp

--- cracking_the_coding_interview/17-3.cpp
+++ cracking_the_coding_interview/17-3.cpp
@@ -4,25 +4,47 @@
 using namespace std;
 
 void randSubSet(vector<int>& nums, size_t newSize, size_t i){
-    if(i == newSize){
+    if(i == nums.size()){
         nums.resize(newSize);
         return;
     }
 
     size_t j = rand() / (RAND_MAX + 1u) / (i + 1);
-    swap(nums[i], nums[j]);
+    if(j < newSize){
+        swap(nums[i], nums[j]);
+    }
     randSubSet(nums, newSize, i + 1);
 }
 
 void randSubSet(vector<int>& nums, size_t newSize){
-    randSubSet(nums, newSize, 0);
+    randSubSet(nums, newSize, newSize + 1);
     return;
 }
 
+vector<int> randSubSetWithForRoop(vector<int>& nums, size_t newSize){
+    auto first = nums.begin();
+    auto last = nums.begin() + newSize;
+    vector<int> subSet(first, last);
+    for(int i = newSize; i < nums.size(); i++){
+        size_t j = rand() / (RAND_MAX + 1u) / (i + 1);
+        if(j < newSize){
+            subSet[j] = nums[i];
+        }
+    }
+    return subSet;
+}
+
 int main(void){
-    vector<int> nums = {345, 6, 312, 43, 56, 1, 36, 638, 49, 50};
-    randSubSet(nums, 5);
-    for (int num: nums){
+    vector<int> nums1 = {345, 6, 312, 43, 56, 1, 36, 638, 49, 50};
+    randSubSet(nums1, 5);
+    for (int num: nums1){
+        cout << num << " ";
+    }
+    cout << endl;
+
+    vector<int> nums2 = {345, 6, 312, 43, 56, 1, 36, 638, 49, 50};
+    vector<int> res = randSubSetWithForRoop(nums2, 5);
+    for (int num: res){
         cout << num << " ";
     }
     cout << endl;
