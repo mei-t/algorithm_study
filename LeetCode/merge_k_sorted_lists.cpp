dd561b9ec09bc7d5aa89ff01345f846ceb10301c
Mei Takahashi <me.7tspr@gmail.com>
2019-12-01 17:34:17 +0900

    Modify merge_k_sorted_lists.cpp

--- LeetCode/merge_k_sorted_lists.cpp
+++ LeetCode/merge_k_sorted_lists.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+// #include <algorithm>
+#include <queue>
 using namespace std;
 
 struct ListNode {
@@ -27,6 +29,60 @@ public:
         }
         ListNode* node = lists[minIndex];
         lists[minIndex] = node->next;
+        node->next = mergeKLists(lists);
+        return node;
+    }
+};
+
+class Solution2 {
+public:
+    function<bool(ListNode*, ListNode*)> cmp = [](ListNode* node1, ListNode* node2){
+        if(!node1){
+            return true;
+        }else if(!node2){
+            return false;
+        }else{
+            return node1->val > node2->val;
+        }
+    };
+    
+    ListNode* mergeKLists(vector<ListNode*>& lists) { 
+        if(lists.empty()){
+            return nullptr;
+        } else if(lists.size() == 1){
+            return lists[0];
+        }
+
+        // struct{
+        //     bool operator() (ListNode* node1, ListNode* node2){
+        //         if(!node1){
+        //             return false;
+        //         }else if(!node2){
+        //             return true;
+        //         }else{
+        //             return node1->val < node2->val;
+        //         }
+        //     }
+        // } nodeLess;
+        // sort(lists.begin(), lists.end(), nodeLess);
+
+        
+        priority_queue<ListNode*, vector<ListNode*>, decltype(cmp)> q(cmp);
+        for(ListNode* node: lists){
+            q.push(node);
+        }
+        ListNode* node = mergeKLists(q);
+        return node;
+    }
+
+    ListNode* mergeKLists(priority_queue<ListNode*, vector<ListNode*>, decltype(cmp)>& lists) { 
+        if(!lists.top()){
+            return nullptr;
+        }
+
+        ListNode* node = lists.top();
+        lists.pop();
+        lists.push(node->next);
         node->next = mergeKLists(lists);
         return node;
     }
