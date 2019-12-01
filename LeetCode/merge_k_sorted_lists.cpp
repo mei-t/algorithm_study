368c6ded84c26464308eb4eddb5bb7bc96618b83
Mei Takahashi <me.7tspr@gmail.com>
2019-12-01 17:50:50 +0900

    Modify merge_k_sorted_lists.cpp

--- LeetCode/merge_k_sorted_lists.cpp
+++ LeetCode/merge_k_sorted_lists.cpp
@@ -36,6 +36,12 @@ public:
 
 class Solution2 {
 public:
+
+    ListNode* mergeKLists(vector<ListNode*>& lists);
+
+    ListNode* mergeKLists(priority_queue<ListNode*, vector<ListNode*>, decltype(cmp)>& lists);
+
+private:
     function<bool(ListNode*, ListNode*)> cmp = [](ListNode* node1, ListNode* node2){
         if(!node1){
             return true;
@@ -45,48 +51,48 @@ public:
             return node1->val > node2->val;
         }
     };
-    
-    ListNode* mergeKLists(vector<ListNode*>& lists) { 
-        if(lists.empty()){
-            return nullptr;
-        } else if(lists.size() == 1){
-            return lists[0];
-        }
-
-        // struct{
-        //     bool operator() (ListNode* node1, ListNode* node2){
-        //         if(!node1){
-        //             return false;
-        //         }else if(!node2){
-        //             return true;
-        //         }else{
-        //             return node1->val < node2->val;
-        //         }
-        //     }
-        // } nodeLess;
-        // sort(lists.begin(), lists.end(), nodeLess);
-
-        
-        priority_queue<ListNode*, vector<ListNode*>, decltype(cmp)> q(cmp);
-        for(ListNode* node: lists){
-            q.push(node);
-        }
-        ListNode* node = mergeKLists(q);
-        return node;
-    }
-
-    ListNode* mergeKLists(priority_queue<ListNode*, vector<ListNode*>, decltype(cmp)>& lists) { 
-        if(!lists.top()){
-            return nullptr;
-        }
-
-        ListNode* node = lists.top();
-        lists.pop();
-        lists.push(node->next);
-        node->next = mergeKLists(lists);
-        return node;
-    }
 };
+
+ListNode* Solution2::mergeKLists(vector<ListNode*>& lists) { 
+    if(lists.empty()){
+        return nullptr;
+    } else if(lists.size() == 1){
+        return lists[0];
+    }
+
+    // struct{
+    //     bool operator() (ListNode* node1, ListNode* node2){
+    //         if(!node1){
+    //             return false;
+    //         }else if(!node2){
+    //             return true;
+    //         }else{
+    //             return node1->val < node2->val;
+    //         }
+    //     }
+    // } nodeLess;
+    // sort(lists.begin(), lists.end(), nodeLess);
+
+    
+    priority_queue<ListNode*, vector<ListNode*>, decltype(cmp)> q(cmp);
+    for(ListNode* node: lists){
+        q.push(node);
+    }
+    ListNode* node = mergeKLists(q);
+    return node;
+}
+
+ListNode* Solution2::mergeKLists(priority_queue<ListNode*, vector<ListNode*>, decltype(cmp)>& lists) { 
+    if(!lists.top()){
+        return nullptr;
+    }
+
+    ListNode* node = lists.top();
+    lists.pop();
+    lists.push(node->next);
+    node->next = mergeKLists(lists);
+    return node;
+}
 
 int main(void){
     return 0;
