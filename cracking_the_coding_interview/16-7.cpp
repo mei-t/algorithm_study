d1915e1e0cf3e3a0e10aff2486850e9443e92328
Mei Takahashi <me.7tspr@gmail.com>
2019-12-02 22:28:05 +0900

    Modify

--- cracking_the_coding_interview/16-7.cpp
+++ cracking_the_coding_interview/16-7.cpp
@@ -16,9 +16,21 @@ int compare(int a, int b){
     return max;
 }
 
+int compare2(int a, int b){
+    bool isAMinus = a & (1 << (SIZE_INT - 1));
+    bool isBMinus = b & (1 << (SIZE_INT - 1));
+    bool isSignDiff = isAMinus ^ isBMinus;
+    bool isAMax = (b - a) & (1 << (SIZE_INT - 1));
+    bool isBMax = (a - b) & (1 << (SIZE_INT - 1));
+    return (!isAMinus * a + !isBMinus * b) * isSignDiff + (isAMax * a + isBMax * b) * !isSignDiff;
+}
+
 int main(void){
     cout << compare(10, 2) << endl; // 10
+    cout << compare2(10, 2) << endl; // 10
     cout << compare(-25, 7) << endl; // 7
+    cout << compare2(-25, 7) << endl; // 7
     cout << compare(-12, -3) << endl; // -3
+    cout << compare2(-12, -3) << endl; // -3
     return 0;
 }
