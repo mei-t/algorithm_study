a53ea1614087602f8b2525ec28e64d7ed2f6d922
Mei Takahashi <me.7tspr@gmail.com>
2019-12-04 17:50:28 +0900

    Fix

--- cracking_the_coding_interview/17-4.cpp
+++ cracking_the_coding_interview/17-4.cpp
@@ -22,13 +22,50 @@ private:
     int num_;
 };
 
-unsigned long lostNum(const vector<binNum>& nums){
+unsigned long lostNum(vector<binNum>& nums, size_t i, bitset<LONG_SIZE> b){
+    if(nums.empty() || i == INT_SIZE){
+        return b.to_ulong();
+    }
+
+    int countOne = 0;
+    int countZero = 0;
+    for(binNum num: nums){
+        if(num.getbit(i)){
+            countOne++;
+        }else{
+            countZero++;
+        }
+    }
+    bool isOneAns = countOne < countZero ? true : false;
+    if(isOneAns){
+        b.set(i);
+    }
+    for(auto it = nums.begin(); it != nums.end(); ){
+        if(it->getbit(i) && !isOneAns){
+            nums.erase(it);
+        }else if(!it->getbit(i) && isOneAns){
+            nums.erase(it);
+        }else{
+            it++;
+        }
+    }
+
+    return lostNum(nums, i + 1, b);
+}
+
+unsigned long lostNum(vector<binNum>& nums){
+    bitset<LONG_SIZE> b;
+    return lostNum(nums, 0, b);
+}
+
+// うまくいかなかった
+unsigned long lostNum2(const vector<binNum>& nums){
     vector<int> countOne(INT_SIZE, 0);
     for(binNum num: nums){
-        cout << "num is " << num.getNum() << endl;
+        // cout << "num is " << num.getNum() << endl;
         for(int i=0; i<INT_SIZE; i++){
             if(num.getbit(i)){
-                cout << "i = " << i << ", countOne[i] = " << countOne[i] << endl;
+                // cout << "i = " << i << ", countOne[i] = " << countOne[i] << endl;
                 countOne[i]++;
             }
         }
@@ -38,8 +75,9 @@ unsigned long lostNum(const vector<binNum>& nums){
     bitset<LONG_SIZE> b;
     for(int i=0; countOne[i] > 0; i++){
         int cycle = size / pow(2, i+1);
+        // この部分にミスあり
         if(countOne[i] != cycle * pow(2, i) + max(0, cycle % (int)pow(2, i))){
-            cout << i << endl;
+            // cout << i << endl;
             b.set(i);
         }
     }
@@ -48,6 +86,12 @@ unsigned long lostNum(const vector<binNum>& nums){
 
 int main(void){
     vector<binNum> nums = {0, 1, 2, 3, 4, 5, 7, 8, 9, 10};
-    cout << lostNum(nums) << endl;
+    cout << lostNum(nums) << endl; // 6
+    nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    cout << lostNum(nums) << endl; // 0
+    nums = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16};
+    cout << lostNum(nums) << endl; // 15
+    nums = {0, 1, 2, 3, 4, 5, 7, 8, 9, 10};
+    cout << lostNum2(nums) << endl; // 6
     return 0;
 }
