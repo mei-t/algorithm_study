c3934dc0d221b12a656d8a607a3356702c0376e7
Mei Takahashi <me.7tspr@gmail.com>
2019-12-08 23:59:49 +0900

    Modify 16-9.cpp

--- cracking_the_coding_interview/16-9.cpp
+++ cracking_the_coding_interview/16-9.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 using namespace std;
 
+int signInversion(int a){
+    int sign = a > 0 ? 1 : -1;
+    int delta = sign;
+    int res = 0;
+    while(a != 0){
+        bool diffSign = (a + delta > 0) != (a > 0);
+        if(a + delta != 0 && diffSign){
+            delta = sign;
+        }
+        res += delta;
+        a += delta;
+        delta += delta;
+    }
+    return res;
+}
+
 int subtract(int a, int b){
-    return a + (~b) + 1;
+    return a + signInversion(b);
+}
+
+int abs(int a){
+    return a > 0 ? a : signInversion(a);
 }
 
 int multi(int sum, int times, int a){
@@ -13,10 +33,12 @@ int multi(int sum, int times, int a){
 }
 
 int multi(int a, int b){
-    bool isMinus = (a > 0) ^ (b > 0);
-    int max = abs(a) > abs(b) ? abs(a) : abs(b);
-    int min = abs(a) > abs(b) ? abs(b) : abs(a);
-    return isMinus ? ~multi(0, min, max) + 1 : multi(0, min, max);
+    bool isMinus = (a > 0) != (b > 0);
+    int absA = abs(a);
+    int absB = abs(b);
+    int max = absA > absB ? absA : absB;
+    int min = absA > absB ? absB : absA;
+    return isMinus ? signInversion(multi(0, min, max)) : multi(0, min, max);
 }
 
 int division(int a, int b, int sum, int times){
@@ -27,8 +49,8 @@ int division(int a, int b, int sum, int times){
 }
 
 int division(int a, int b){
-    bool isMinus = (a < 0) ^ (b < 0);
-    return isMinus ? ~division(abs(a), abs(b), 0, 0) + 1 : division(abs(a), abs(b), 0, 0);
+    bool isMinus = (a < 0) != (b < 0);
+    return isMinus ? signInversion(division(abs(a), abs(b), 0, 0)) : division(abs(a), abs(b), 0, 0);
 }
 
 int main(void){
