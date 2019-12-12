4858f4f073181031bb61c31ca2e34e6d7f53f66d
Mei Takahashi <me.7tspr@gmail.com>
2019-12-12 16:19:11 +0900

    Add another approach to 2-8.cpp

--- cracking_the_coding_interview/2-8.cpp
+++ cracking_the_coding_interview/2-8.cpp
@@ -23,6 +23,25 @@ Node* findLoop(Node* root){
     return findLoop(root, &nodeSet);
 }
 
+Node* findLoop2(Node* fromStart, Node* fromMeetingPoint){
+    if(fromStart == fromMeetingPoint){
+        return fromStart;
+    }
+    return findLoop2(fromStart->next, fromMeetingPoint->next);
+}
+
+Node* findMeetingPoint2(Node* slow, Node* fast){
+    if(slow == fast){
+        return slow;
+    }
+    return findMeetingPoint2(slow->next, fast->next->next);
+}
+
+Node* findLoop2(Node* root){
+    Node* meetingPoint = findMeetingPoint2(root->next, root->next->next);
+    return findLoop2(root, meetingPoint);
+}
+
 Node* insert(const vector<int>& nums, size_t i, size_t roopIndex, Node* roopNode = nullptr){
     if(i == nums.size() - 1){
         return roopNode;
@@ -41,5 +60,7 @@ int main(void){
     Node* root = insert(nums, 0, 5);
     Node* roopNode = findLoop(root);
     cout << roopNode->val << endl;
+    Node* roopNode2 = findLoop2(root);
+    cout << roopNode2->val << endl;
     return 0;
 }
