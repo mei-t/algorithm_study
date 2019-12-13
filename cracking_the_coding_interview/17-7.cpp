542a9f90b7f7b49a80efed59cc923a1b8646151d
Mei Takahashi <me.7tspr@gmail.com>
2019-12-14 00:02:56 +0900

    Add 17-7.cpp

---
+++ cracking_the_coding_interview/17-7.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+unordered_map<string, int> trueNameCount(const vector<pair<string, string>>& synonyms, const unordered_map<string, int>& names){
+    unordered_map<string, string> nameGroup;
+    unordered_map<string, vector<string>> dict;
+    for(const pair<string, string>& synonym: synonyms){
+        auto itFirst = nameGroup.find(synonym.first);
+        auto itSecond = nameGroup.find(synonym.second);
+        if(itFirst == nameGroup.end() && itSecond == nameGroup.end()){
+            nameGroup.insert({synonym.first, synonym.first});
+            nameGroup.insert({synonym.second, synonym.first});
+            dict.insert({synonym.first, {synonym.first, synonym.second}});
+        }else if(itFirst != nameGroup.end() && itSecond != nameGroup.end() && itFirst->second != itSecond->second){
+            vector<string> change = dict[itFirst->second].size() < dict[itSecond->second].size() ? dict[itFirst->second] : dict[itSecond->second];
+            string newName = dict[itFirst->second].size() < dict[itSecond->second].size() ? itSecond->second : itFirst->second;
+            for(string name : change){
+                nameGroup[name] = newName;
+            }
+            // vectorではなく連結リストならmergeにO(1)しかかからない気がする
+            dict[newName].insert(dict[newName].end(), change.begin(), change.end());
+        }else if(itFirst != nameGroup.end() && itSecond == nameGroup.end()){
+            nameGroup.insert({synonym.second, itFirst->second});
+            dict[itFirst->second].push_back(synonym.second);
+        }else if(itFirst == nameGroup.end() && itSecond != nameGroup.end()){
+            nameGroup.insert({synonym.first, itSecond->second});
+            dict[itSecond->second].push_back(synonym.first);
+        }
+    }
+
+    unordered_map<string, int> nameMap;
+    for(auto it = names.begin(); it != names.end(); it++){
+        if(nameMap.find(nameGroup[it->first]) == nameMap.end()){
+            nameMap.insert({nameGroup[it->first], 0});
+        }
+        nameMap[nameGroup[it->first]] += it->second;
+    }
+    return nameMap;
+}
+
+int main(void){
+    return 0;
+}
