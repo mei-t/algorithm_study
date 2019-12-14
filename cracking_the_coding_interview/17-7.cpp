b8bf362470e5665bba2a7a7fc51ae07f5353027e
Mei Takahashi <me.7tspr@gmail.com>
2019-12-14 15:23:03 +0900

    Add another approach to 17-7.cpp

--- cracking_the_coding_interview/17-7.cpp
+++ cracking_the_coding_interview/17-7.cpp
@@ -41,6 +41,67 @@ unordered_map<string, int> trueNameCount(const vector<pair<string, string>>& syn
     return nameMap;
 }
 
+struct Graph{
+    string name;
+    int count;
+    bool isVisited;
+    Graph* next;
+    vector<Graph*> neighbers;
+};
+
+Graph* createNewGraph(const vector<pair<string, int>>& names, size_t i, unordered_map<string, Graph*>* nameMap){
+    if(i == names.size()){
+        return nullptr;
+    }
+    Graph* graph = new Graph{names[i].first, names[i].second, false, nullptr, vector<Graph*>()};
+    graph->next = createNewGraph(names, i+1, nameMap);
+    nameMap->insert({names[i].first, graph});
+    return graph;
+}
+
+unordered_map<string, int> trueNameCount2(const vector<pair<string, string>>& synonyms, const vector<pair<string, int>>& names){
+    unordered_map<string, Graph*> nameMap;
+    Graph* root = createNewGraph(names, 0, &nameMap);
+
+    for(const auto& synonym: synonyms){
+        if(!nameMap[synonym.first]){
+            nameMap[synonym.first] = new Graph{synonym.first, 0, false, nullptr, vector<Graph*>()};
+        }
+        if(!nameMap[synonym.second]){
+            nameMap[synonym.second] = new Graph{synonym.first, 0, false, nullptr, vector<Graph*>()};
+        }
+        nameMap[synonym.first]->neighbers.push_back(nameMap[synonym.second]);
+        nameMap[synonym.second]->neighbers.push_back(nameMap[synonym.first]);
+    }
+
+    unordered_map<string, int> ans;
+    Graph* current = root->next;
+    Graph* prev;
+    while(current){
+        if(current->isVisited){
+            current = current->next;
+            continue;
+        }
+        current->isVisited = true;
+        ans[current->name] = current->count;
+        for(Graph* neighber : current->neighbers){
+            if(neighber->isVisited){
+                continue;
+            }
+            neighber->isVisited = true;
+            ans[current->name] += neighber->count;
+            if(!neighber->next){
+                delete(neighber);
+            }
+        }
+        prev = current;
+        current = current->next;
+        delete(prev);
+    }
+    return ans;
+}
+
+
 int main(void){
     unordered_map<string, int> names = {
         {"John", 15},
@@ -56,6 +117,18 @@ int main(void){
         make_pair("Chris", "Christopher")
     };
     unordered_map<string, int> ans = trueNameCount(synonyms, names);
+    for(auto it : ans){
+        cout << it.first << ", " << it.second << endl;
+    }
+
+    vector<pair<string, int>> names2 = {
+        {"John", 15},
+        {"Jon", 12},
+        {"Chris", 13},
+        {"Kris", 4},
+        {"Christopher", 19}
+    };
+    ans = trueNameCount2(synonyms, names2);
     for(auto it : ans){
         cout << it.first << ", " << it.second << endl;
     }
