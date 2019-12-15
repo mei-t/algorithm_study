bf105eb8097f446a881f146e108acb0d2125045f
Mei Takahashi <me.7tspr@gmail.com>
2019-12-15 12:37:16 +0900

    Modify

--- cracking_the_coding_interview/8-6.cpp
+++ cracking_the_coding_interview/8-6.cpp
@@ -41,6 +41,9 @@ public:
             cout << "move failed" << endl;
             return;
         }
+        cout << "stack[0].size() = " << stacks[0].size() << endl;
+        cout << "stack[1].size() = " << stacks[1].size() << endl;
+        cout << "stack[2].size() = " << stacks[2].size() << endl;
         cout << "Move " << stacks[i].top() << " from " << i << " to " << j << "." << endl;
         stacks[j].push(stacks[i].top());
         stacks[i].pop();
@@ -57,12 +60,18 @@ public:
         stack<int>& stack2 = stacks[1];
         stack<int>& stack3 = stacks[2];
 
-        if(stack1.empty()){
-            cout << "stack1 is empty" << endl;
-            empty = 0;
-            max = stack2.top() > stack3.top() ? 1 : 2;
-            return;
-        }else if(stack2.empty()){
+        cout << "stack1.size() = " << stack1.size() << endl;
+        cout << "stack2.size() = " << stack2.size() << endl;
+        cout << "stack3.size() = " << stack3.size() << endl;
+        cout << stack1.empty() << " " << stack2.empty() << " " << stack3.empty() << endl;
+
+        // if(stack1.empty()){
+        //     cout << "stack1 is empty" << endl;
+        //     empty = 0;
+        //     max = stack2.top() > stack3.top() ? 1 : 2;
+        //     return;
+        // }else 
+        if(stack2.empty()){
             cout << "stack2 is empty" << endl;
             empty = 1;
             max = stack1.top() > stack3.top() ? 0 : 2;
@@ -75,7 +84,7 @@ public:
         }
         int tmp = stack1.top() < stack2.top() ? 0 : 1;
         min = stacks[tmp].top() < stack3.top() ? tmp : 2;
-        if(tmp == min){
+        if(tmp != min){
             mid = tmp;
             return;
         }
@@ -91,14 +100,68 @@ private:
     stack<int> stacks[NB_STACKS];
 };
 
+class Hanoi2{
+public:
+    Hanoi2(int k);
+    void start();
+    void moveToDisk(stack<int>* origin, stack<int>* buffer, stack<int>* disk, size_t i);
+    void moveTop(stack<int>* origin, stack<int>* disk);
+    void finish();
+private:
+    stack<int> origin;
+    stack<int> buffer;
+    stack<int> disk;
+};
+
+Hanoi2::Hanoi2(int k){
+    for(int i = k; i > 0; i--){
+        origin.push(i);
+    }
+}
+void Hanoi2::start(){
+    moveToDisk(&origin, &buffer, &disk, origin.size());
+    finish();
+}
+
+void Hanoi2::moveToDisk(stack<int>* origin, stack<int>* buffer, stack<int>* disk, size_t i){
+    if(i == 0){
+        return;
+    }
+    moveToDisk(origin, disk, buffer, i - 1);
+    moveTop(origin, disk);
+    moveToDisk(buffer, origin, disk, i - 1);
+}
+
+void Hanoi2::moveTop(stack<int>* origin, stack<int>* disk){
+    disk->push(origin->top());
+    origin->pop();
+    return;
+}
+
+void Hanoi2::finish(){
+    while(!disk.empty()){
+        cout << disk.top() << " ";
+        disk.pop();
+    }
+    cout << endl;
+    return;
+}
+
 int main(void){
-    cout << "aaaaaaaaa" << endl;
-    cout << "main start" << endl;
-    TowerOfHanoi hanoi1(3);
-    hanoi1.start();
-    cout << "Hanoi1 finished." << endl;
+    // cout << "aaaaaaaaa" << endl;
+    // cout << "main start" << endl;
+    // TowerOfHanoi hanoi1(3);
+    // hanoi1.start();
+    // cout << "Hanoi1 finished." << endl;
     // TowerOfHanoi hanoi2(10);
     // hanoi2.start();
     // cout << "Hanoi2 finished." << endl;
+
+    Hanoi2 hanoi1(3);
+    hanoi1.start();
+
+    Hanoi2 hanoi2(10);
+    hanoi2.start();
+
     return 0;
 }
