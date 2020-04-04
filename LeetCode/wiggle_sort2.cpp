854341a3f31d304842f50e3456cfbf2690e0c403
Mei Takahashi <me.7tspr@gmail.com>
2020-04-04 21:36:02 +0900

    Clean wiggle_sort2.cpp.

--- LeetCode/wiggle_sort2.cpp
+++ LeetCode/wiggle_sort2.cpp
@@ -7,20 +7,11 @@ public:
     void wiggleSort(vector<int>& nums) {
         vector<int> sorted(nums);
         sort(sorted.begin(), sorted.end());
-        output(sorted);
-        // for(int i=0; i<nums.size()/2; i++){
-        //     nums[i*2] = sorted[i];
-        //     nums[i*2 + 1] = sorted[nums.size()/2 + nums.size()%2 + i];
-        // }
+        // output(sorted);
         for(int i=nums.size()-1, j=0, k=(nums.size() - 1)/2 + 1; i>=0; i--){
             nums[i] = sorted[(i%2 ? k++: j++)];
         }
-        // if(nums.size()%2){
-        //     nums[nums.size() - 1] = sorted[nums.size()/2];
-        //     cout << "nums[" << nums.size() - 1 << "]" << endl;
-        //     cout << "sorted[" << nums.size()/2 + 1 << "] = " << sorted[nums.size()/2 + 1] << endl;
-        // }
-        output(nums);
+        // output(nums);
     }
 private:
     void output(vector<int>& input){
