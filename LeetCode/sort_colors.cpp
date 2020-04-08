18ed152699be98eee203bd54e23dc5cd3c3d53c3
Mei Takahashi <me.7tspr@gmail.com>
2020-04-08 22:56:32 +0900

    Add sort_colors.cpp

---
+++ LeetCode/sort_colors.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <list>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        list<int> input(nums.begin(), nums.end());
+        auto it = input.begin();
+        for(int i=0; i < input.size(); i++){
+            if(*it == 0){
+                it = input.erase(it);
+                input.push_front(0);
+            }else if(*it == 1){
+                it++;
+            }else{
+                it = input.erase(it);
+                input.push_back(2);
+            }
+        }
+        it = input.begin();
+        for(int i=0; i<nums.size(); i++, it++){
+            nums[i] = *it;
+        }
+        output(nums);
+    }
+private:
+    void output(vector<int> nums){
+        for(int num: nums){
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+};
+
+int main(void){
+    vector<int> nums = {2, 0, 2, 1, 1, 0};
+    Solution sol;
+    sol.sortColors(nums);
+    return 0;
+}
