2aca8bfc05cf4f888d7124a6795a79e0ec796bc2
Mei Takahashi <me.7tspr@gmail.com>
2020-04-16 15:26:23 +0900

    Add another solution to copy_list_with_random_pointer.cpp

--- LeetCode/copy_list_with_random_pointer.cpp
+++ LeetCode/copy_list_with_random_pointer.cpp
@@ -16,7 +16,7 @@ public:
     }
 };
 
-class Solution {
+class Solution1 {
 public:
     Node* copyRandomList(Node* head) {
        vector<Node*> nodeList;
@@ -46,6 +46,44 @@ private:
             copyNode->random = nodeList[node->random->val];
         }
         makeRandomLink(node->next, copyNode->next, nodeList);
+    }
+};
+
+class Solution2 {
+public:
+    Node* copyRandomList(Node* head) {
+        makeCopyNode(head);
+        makeRandomLink(head);
+        return separateCopyFromOriginal(head);
+    }
+private:
+    void makeCopyNode(Node* node){
+        if(!node){
+            return;
+        }
+        Node* copyNode = new Node(node->val);
+        copyNode->next = node->next;
+        node->next = copyNode;
+        makeCopyNode(copyNode->next);
+    }
+
+    void makeRandomLink(Node* node){
+        if(!node){
+            return;
+        }
+        Node* copyNode = node->next;
+        copyNode->random = node->random ? node->random->next : nullptr;
+        makeRandomLink(copyNode->next);
+    }
+
+    Node* separateCopyFromOriginal(Node* node){
+        if(!node){
+            return nullptr;
+        }
+        Node* copyNode = node->next;
+        node->next = copyNode->next;
+        copyNode->next = separateCopyFromOriginal(copyNode->next);
+        return copyNode;
     }
 };
 
