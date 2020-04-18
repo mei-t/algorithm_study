5c4350f9594926d4be6deb624e934f5e57180ae1
Mei Takahashi <me.7tspr@gmail.com>
2020-04-19 00:44:42 +0900

    Modify 1-9.cpp.

--- cracking_the_coding_interview/1-9.cpp
+++ cracking_the_coding_interview/1-9.cpp
@@ -2,28 +2,35 @@
 using namespace std;
 
 bool isSubstring(string& s1, string& s2){
-    if(s1.size() != s2.size()){
-        return false;
-    }
-    int size = s1.size();
-    for(int i = 0; i < size; i++){
-        bool isAllSame = true;
-        for(int j = 0; j < size; j++){
-            if(s1[j] != s2[(i + j) % size]){
-                isAllSame = false;
+    string longS = s1.size() > s2.size() ? s1 : s2;
+    string shortS = s1.size() > s2.size() ? s2 : s1;
+    for(int i = 0; i <= longS.size() - shortS.size(); i++){
+        if(longS[i] != shortS[0]){
+            continue;
+        }
+        for(int j = 0; j < shortS.size(); j++){
+            if(longS[i + j] != shortS[j]){
                 break;
             }
-        }
-        if(isAllSame){
-            return true;
+            if(j == shortS.size() - 1){
+                return true;
+            }
         }
     }
     return false;
 }
 
+bool isRotation(string& s1, string& s2){
+    if(s1.size() != s2.size()){
+        return false;
+    }
+    string doubleS1 = s1 + s1;
+    return isSubstring(doubleS1, s2);
+}
+
 int main(void){
     string s1 = "waterbottle";
     string s2 = "erbottlewat";
-    cout << isSubstring(s1, s2) << endl;
+    cout << isRotation(s1, s2) << endl;
     return 0;
 }
