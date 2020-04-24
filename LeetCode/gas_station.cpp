08e18a69e0424285cad4cd143ac29cdfe6a11fd5
Mei Takahashi <me.7tspr@gmail.com>
2020-04-24 23:14:05 +0900

    Add better solution and test.

--- LeetCode/gas_station.cpp
+++ LeetCode/gas_station.cpp
@@ -4,7 +4,30 @@ using namespace std;
 
 class Solution {
 public:
-    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+    int canCompleteCircuit(const vector<int>& gas, const vector<int>& cost) {
+        int total = 0;
+        int tank = 0;
+        int start = 0;
+        for(int i = 0; i < gas.size(); i++){
+            int tmp = gas[i] - cost[i];
+            total += tmp;
+            tank += tmp;
+            if(tank < 0){
+                start = i + 1;
+                tank = 0;
+            }
+        }
+        if(total < 0){
+            return -1;
+        }
+        return start;
+    }
+};
+
+// class Solution is better solution.
+class Solution2 {
+public:
+    int canCompleteCircuit(const vector<int>& gas, const vector<int>& cost) {
         vector<int> diff;
         int total = 0;
         for(int i = 0; i < gas.size(); i++){
@@ -43,5 +66,9 @@ private:
 };
 
 int main(void){
+    vector<int> gas = { 1, 2, 3, 4, 5 };
+    vector<int> cost = { 3, 4, 5, 1, 2 };
+    Solution sol;
+    cout << sol.canCompleteCircuit(gas, cost) << endl;
     return 0;
 }
