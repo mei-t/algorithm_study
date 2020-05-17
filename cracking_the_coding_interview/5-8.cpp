8430b460d733ae67b2cbbcd412fc05ac0776209f
Mei Takahashi <me.7tspr@gmail.com>
2020-05-17 17:24:44 +0900

    Fix

--- cracking_the_coding_interview/5-8.cpp
+++ cracking_the_coding_interview/5-8.cpp
@@ -5,10 +5,13 @@ using namespace std;
 
 void drawLine(vector<char>& screen, int width, int x1, int x2, int y){
     int stride = width/8;
-    int line_y = (y-1) * stride;
+    int line_y = y * stride;
     int start_block = x1/8;
     int end_block = x2/8;
     if(start_block == end_block){
+        cout << (0xFF >> (x1%8)) << endl;
+        cout << (0xFF << (x2%8)) << endl;
+        cout << ((0xFF >> ((x1-1)%8)) & (0xFF << (x2%8))) << endl;
         screen[line_y + start_block] = (0xFF >> ((x1-1)%8)) & (0xFF << (x2%8));
         return;
     }
@@ -35,8 +38,11 @@ void output(vector<char>& screen, int width){
 }
 
 int main(void){
-    vector<char> screen(25, 0);
-    drawLine(screen, 40, 3, 28, 3);
-    output(screen, 40);
+    vector<char> screen1(25, 0);
+    drawLine(screen1, 40, 3, 28, 3);
+    output(screen1, 40);
+    vector<char> screen2(25, 0);
+    drawLine(screen2, 40, 3, 5, 3);
+    output(screen2, 40);
     return 0;
 }
