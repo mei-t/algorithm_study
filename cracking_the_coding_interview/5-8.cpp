18644636d36edf186e8a01200a01bc240cbdd1a3
Mei Takahashi <me.7tspr@gmail.com>
2020-05-17 11:18:15 +0900

    Update

--- cracking_the_coding_interview/5-8.cpp
+++ cracking_the_coding_interview/5-8.cpp
@@ -3,30 +3,33 @@
 #include <vector>
 using namespace std;
 
-void drawLine(vector<byte>& screen, int width, int x1, int x2, int y){
-    byte b{255};
+void drawLine(vector<char>& screen, int width, int x1, int x2, int y){
     if(x1/8 == x2/8){
-        screen[(y-1) * width/8 + x1/8] = (b >> (x1%8)) & (b << (x2%8));
+        screen[(y-1) * width/8 + x1/8] = (~0 >> (x1%8)) & (~0 << (x2%8));
         return;
     }
-    cout << to_integer<int>(b) << endl;
-    screen[(y-1) * width/8 + x1/8] = b >> (x1%8);
-    cout << to_integer<int>(b) << endl;
+    cout << "x1%8 = " << x1%8 << endl;
+    cout << "~0 >> (x1%8) = " << (~0 >> (x1%8)) << endl;
+    cout << ~0 << endl;
+    cout << "0xFF >> (x1%8) = " << (0xFF >> (x1%8)) << endl;
+    cout << 0xFF << endl;
+    screen[(y-1) * width/8 + x1/8] = ~0 >> (x1%8);
     for(int i = x1/8 + 1; i < x2/8; i++){
-        screen[(y-1)*width/8 + i] = b;
+        screen[(y-1)*width/8 + i] = 0xFF;
     }
-    screen[(y-1) * width/8 + x2/8] = b << (x2%8);
+    screen[(y-1) * width/8 + x2/8] = ~0 << (x2%8);
 }
 
-void outputByte(byte b){
+void outputByte(char c){
     for(int i=0; i<8; i++){
-        cout << (to_integer<int>(b) & (1 << (8-i))) << " ";
+        cout << (c & (1 << (8-i))) << " ";
     }
 }
 
-void output(vector<byte>& screen, int width){
+void output(vector<char>& screen, int width){
     for(int i = 0; i < screen.size()/(width/8); i++){
         for(int j = 0; j < width/8; j++){
+            cout << "screen[" << i+j << "] = " << screen[i+j] << endl;
             outputByte(screen[i+j]);
         }
         cout << endl;
@@ -34,8 +37,7 @@ void output(vector<byte>& screen, int width){
 }
 
 int main(void){
-    byte b{0};
-    vector<byte> screen(25, b);
+    vector<char> screen(25, 0);
     drawLine(screen, 40, 3, 28, 3);
     output(screen, 40);
     return 0;
