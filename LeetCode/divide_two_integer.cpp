ce68107f283a07d3a4fb66c09adc6801ab5c01f2
Mei Takahashi <me.7tspr@gmail.com>
2020-05-18 14:45:44 +0900

    Modify

--- LeetCode/divide_two_integer.cpp
+++ LeetCode/divide_two_integer.cpp
@@ -4,30 +4,30 @@ using namespace std;
 
 class Solution {
 public:
-    long divide(long dividend, int divisor) {
-        bool isPositive = true;
-        if((dividend < 0) ^ (divisor < 0)){
-            isPositive = false;
-        }
-        long num = abs(dividend);
+    int divide(long long dividend, int divisor) {
+        if(dividend == INT_MIN && divisor == -1)
+            return INT_MAX;
+        int sign = (dividend < 0) ^ (divisor < 0) ? -1: 1;
+        long long num = abs(dividend);
         divisor = abs(divisor);
-        long ans = 0;
+        long long ans = 0;
         while(num >= divisor){
-            // cout << num << " " << divisor << endl;
-            num -= divisor;
-            ans++;
+            long long div = divisor;
+            int tmp = 1;
+            while(num >= (div << 1)){
+                div <<= 1;
+                tmp <<= 1;
+            }
+            num -= div;
+            ans += tmp;
         }
-        cout << ans << endl;
-        cout << "isPositive = " << isPositive << endl;
-        ans = isPositive ? ans : -1 * ans;
-        cout << ans << endl;
-        return ans;
-        // return isPositive ? ans : -1 * ans;
+        return sign * ans;
     }
 };
 
 int main(void){
     Solution sol;
-    cout << sol.divide(-2147483648, -1) << endl;;
+    cout << sol.divide(-2147483648, 1) << endl;;
+    // cout << sol.divide(-2147483648, -1) << endl;;
     return 0;
 }
