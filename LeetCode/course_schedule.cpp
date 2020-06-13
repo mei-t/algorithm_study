eb358d456694339ed9e9406754b9a813421a99c2
Mei Takahashi <me.7tspr@gmail.com>
2020-06-13 09:50:50 +0900

    Modify

--- LeetCode/course_schedule.cpp
+++ LeetCode/course_schedule.cpp
@@ -2,62 +2,56 @@
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
+#include <stack>
 using namespace std;
 
-// struct Node{
-//     int val;
-//     Node* prev;
-// };
 
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        for(vector<int>& prerequisite: prerequisites){
-            for(int i = 0; i < prerequisite.size() - 1; i++){
+        for(vector<int>& prerequisite: prerequisites) {
+            for(int i = 0; i < prerequisite.size() - 1; i++) {
                 int cur = prerequisite[i];
-                if(numMap.find(cur) == numMap.end()){
+                int next = prerequisite[i + 1];
+                if(numMap.find(cur) == numMap.end())
                     numMap.insert({cur, vector<int>()});
-                }
-                numMap[cur].push_back(prerequisite[i + 1]);
+                if(indeg.find(next) == indeg.end())
+                    indeg.insert({next, 0});
+                numMap[cur].push_back(next);
+                indeg[next]++;
             }
         }
-        for(auto it: numMap){
-            if(!search(it.first, unordered_set<int>()))
-                return false;
+        for(auto it: numMap) {
+            int num = it.first;
+            if(indeg.find(num) == indeg.end())
+                s.push(num);
         }
-        return true;
+        while(!s.empty()) {
+            int num = s.top();
+            s.pop();
+            for(int next: numMap[num]) {
+                indeg[next]--;
+                if(indeg[next] == 0)
+                    s.push(next);
+            }
+            numMap.erase(num);
+        }
+        return numMap.empty();
     }
 private:
     unordered_map<int, vector<int>> numMap;
-    bool search(int num, unordered_set<int> numSet){
-        numSet.insert(num);
-        for(int next: numMap[num]){
-            if(numSet.find(next) != numSet.end() || !search(next, numSet))
-                return false;
-        }
-        return true;
-    }
+    unordered_map<int, int> indeg;
+    stack<int> s;
 };
 
-// class Solution {
-// public:
-//     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-//         Node* prev = nullptr;
-//         unordered_map<int, Node*> nodeMap;
-//         for(vector<int>& prerequisite: prerequisites){
-//             unordered_set<int> numSet;
-//             for(int i = prerequisite.size() - 1; i >= 0; i--){
-//                 int num = prerequisite[i];
-//                 if(nodeMap.find())
-//                 Node* node(num, prev);
-//                 nodeMap.insert({num, node});
-//                 prev = current;
-//                 numSet.insert(num);
-//             }
-//         }
-//     }
-// };
 
 int main(void){
+    Solution sol;
+    vector<vector<int>> prerequisites = {
+        {1, 0},
+        {0, 1}
+    };
+    bool canFinish = sol.canFinish(2, prerequisites);
+    cout << (canFinish ? "true" : "false") << endl;
     return 0;
 }
