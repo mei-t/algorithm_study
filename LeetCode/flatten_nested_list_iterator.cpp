62cfaf6587a464a4c0a9f98788988f62d4923af4
Mei Takahashi <me.7tspr@gmail.com>
2020-06-23 12:05:11 +0900

    Fix

--- LeetCode/flatten_nested_list_iterator.cpp
+++ LeetCode/flatten_nested_list_iterator.cpp
@@ -15,7 +15,7 @@ class NestedInteger {
     int getInteger() const;
     // Return the nested list that this NestedInteger holds, if it holds a nested list
     // The result is undefined if this NestedInteger holds a single integer
-    const vector<NestedInteger> &getList() const;
+    vector<NestedInteger> &getList() const;
 };
 
 class NestedIterator {
