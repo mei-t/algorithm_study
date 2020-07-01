b957c13bf8f0f71990523b4ce81c5162a90b406f
Mei Takahashi <me.7tspr@gmail.com>
2020-07-01 11:32:17 +0900

    Add another solution.

--- cracking_the_coding_interview/10-11.cpp
+++ cracking_the_coding_interview/10-11.cpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 using namespace std;
 
-vector<int> topAndBottom(vector<int>& nums) {
+void topAndBottom(vector<int>& nums) {
     sort(nums.begin(), nums.end());
     int mid = nums.size() / 2;
     vector<int> small(nums.begin(), nums.begin() + mid);
@@ -11,16 +11,28 @@ vector<int> topAndBottom(vector<int>& nums) {
     for(int i = 0; i < nums.size(); i++){
         nums[i] = (i % 2 == 0 ? large[i/2] : small[i/2]);
     }
-    return nums;
+    return;
 }
 
-vector<int> topAndBottom2(vector<int>& nums) {
+void topAndBottom2(vector<int>& nums) {
     sort(nums.begin(), nums.end());
     for(int i = 1; i < nums.size(); i++) {
         if(i % 2)
             swap(nums[i], nums[i - 1]);
     }
-    return nums;
+    return;
+}
+
+void topAndBottom3(vector<int>& nums) {
+    for(int i = 0; i < nums.size(); i += 2) {
+        int max = i;
+        if(i != 0)
+            max = (nums[i] > nums[i - 1] ? i : i - 1);
+        if(i != nums.size() - 1)
+            max = (nums[max] > nums[i + 1] ? max : i + 1);
+        if(i != max)
+            swap(nums[i], nums[max]);
+    }
 }
 
 void output(vector<int> nums) {
@@ -37,5 +49,8 @@ int main(void) {
     vector<int> nums2 = {5, 3, 1, 2, 3};
     topAndBottom2(nums2);
     output(nums2);
+    vector<int> nums3 = {5, 3, 1, 2, 3};
+    topAndBottom3(nums3);
+    output(nums3);
     return 0;
 }
