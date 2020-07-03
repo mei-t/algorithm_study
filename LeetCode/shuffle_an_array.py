bd5b68123f6be9b28d08097797ef109fb6dc2b51
Mei Takahashi <me.7tspr@gmail.com>
2020-07-03 21:47:50 +0900

    Add shuffle_an_array.py.

---
+++ LeetCode/shuffle_an_array.py
@@ -0,0 +1,25 @@
+class Solution:
+
+    def __init__(self, nums):
+        self.nums = nums
+        
+
+    # Resets the array to its original configuration and return it.
+    def reset(self):
+        return self.nums
+
+
+    # Returns a random shuffling of the array.
+    def shuffle(self):
+        res = self.nums
+        for i in range(len(res)):
+            index = random.randint(0, i)
+            res[index], res[i] = res[i], res[index]
+        return res
+        
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
