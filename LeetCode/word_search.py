899cdf49b5ba2aeebf07a539c2c76a8cec635fab
Mei Takahashi <me.7tspr@gmail.com>
2020-07-07 21:06:16 +0900

    Modify

--- LeetCode/word_search.py
+++ LeetCode/word_search.py
@@ -5,13 +5,9 @@ class Solution:
         if len(board) == 0:
             return word == 0
 
-        print("start")
         for i in range(len(board)):
             for j in range(len(board[0])):
-                # import pdb; pdb.set_trace()
-                print(board[i][j])
                 if word[0] == board[i][j]:
-                    print("word[0] == board[i][j]")
                     if self.findStr(board, word, i, j, 0, set()):
                         return True
 
@@ -19,16 +15,14 @@ class Solution:
     
 
     def findStr(self, board, word, i, j, index, posSet):
-        # import pdb; pdb.set_trace()
         if index == len(word):
             return True
         if i >= len(board) or j >= len(board[0]) or i < 0 or j < 0:
             return False
-        if  (j * len(board[0]) + i) in posSet or word[index] != board[i][j]:
+        if  str(i) + " " + str(j) in posSet or word[index] != board[i][j]:
             return False
         
-        posSet.add(j * len(board[0]) + i)
-        # import pdb; pdb.set_trace()
+        posSet.add(str(i) + " " + str(j))
         up = self.findStr(board, word, i - 1, j, index + 1, posSet.copy())
         if up:
             return True
@@ -66,9 +60,9 @@ class Test(unittest.TestCase):
     word2 = "ABCESEEEFS"
     word3 = "abaabbbaaaaababbbaaaaabbbaab"
 
-    # def test1(self):
-    #     sol = Solution()
-    #     self.assertTrue(sol.exist(self.board1, self.word1))
+    def test1(self):
+        sol = Solution()
+        self.assertTrue(sol.exist(self.board1, self.word1))
 
     def test2(self):
         sol = Solution()
