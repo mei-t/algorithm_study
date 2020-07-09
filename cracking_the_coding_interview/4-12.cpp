d0960be772ed0885d9ca73e559bb2e69ceba47bf
Mei Takahashi <me.7tspr@gmail.com>
2020-07-09 18:28:49 +0900

    Add test and modify

--- cracking_the_coding_interview/4-12.cpp
+++ cracking_the_coding_interview/4-12.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <queue>
+#include <vector>
 using namespace std;
+#define NULLPTR INT_MAX + 1
 
 struct Node{
     int val;
@@ -20,10 +22,11 @@ int countRoute(Node* node, int sum, int given, queue<int>& q){
         int cur = q.front();
         q.pop();
         q.push(cur);
-        curSum += cur;
+        curSum -= cur;
         if(curSum == given)
             res++;
     }
+    q.push(node->val);
     queue<int> c(q);
     res += countRoute(node->left, sum, given, q);
     res += countRoute(node->right, sum, given, c);
@@ -35,6 +38,23 @@ int countRoute(Node* root, int given){
     return countRoute(root, 0, given, q);
 }
 
+Node* buildTree(const vector<long>& nums, int index) {
+    if(index >= nums.size() || nums[index] == NULLPTR)
+        return nullptr;
+    Node* node = new Node{(int)nums[index]};
+    node->left = buildTree(nums, index * 2 + 1);
+    node->right = buildTree(nums, index * 2 + 2);
+    return node;
+}
+
+Node* buildTree(const vector<long>& nums) {
+    return buildTree(nums, 0);
+}
+
 int main(void){
+    vector<long> nums = {10, 5, -3, 3, 2, NULLPTR, 11, 3, -2, NULLPTR, 1};
+    Node* root = buildTree(nums);
+    int res = countRoute(root, 8);
+    cout << res << endl;
     return 0;
 }
