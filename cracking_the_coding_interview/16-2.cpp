fe877afa42f8c2f4049ea31da13a92734e888308
Mei Takahashi <me.7tspr@gmail.com>
2020-07-16 12:46:42 +0900

    Modify

--- cracking_the_coding_interview/16-2.cpp
+++ cracking_the_coding_interview/16-2.cpp
@@ -2,11 +2,16 @@
 #include <unordered_map>
 #include <vector>
 #include <sstream>
+#include <algorithm>
+#include <cctype>
 using namespace std;
 
 class WordFreq{
 public:
-    WordFreq(string s) : s(s) {}
+    WordFreq(string input) : s(input) {
+        transform(s.begin(), s.end(), s.begin(), 
+            [](unsigned char c) -> unsigned char { return std::tolower(c); });
+    }
     double compute(string target){
         if(wordMap.find(target) != wordMap.end())
             return wordMap[target];
@@ -33,5 +38,6 @@ int main(void){
     string s = "A new door to the unbelievable, to the possible, a new day that can always bring you anything if you have no objection to it.";
     WordFreq wf(s);
     cout << wf.compute("to") << endl;
+    cout << wf.compute("a") << endl;
     return 0;
 }
