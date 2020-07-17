23ba59c4a4bef765787b2fa61007cd33527ea50f
Mei Takahashi <me.7tspr@gmail.com>
2020-07-17 11:55:15 +0900

    Add another solution.

--- cracking_the_coding_interview/16-2.cpp
+++ cracking_the_coding_interview/16-2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 #include <sstream>
 #include <algorithm>
@@ -25,7 +26,9 @@ public:
                 count += (word == target ? 1 : 0);
             }
         }
-        return count / sLen;
+        double ans = count / sLen;
+        wordMap.insert({target, ans});
+        return ans;
     }
 
 private:
@@ -33,10 +36,45 @@ private:
     string s;
 };
 
+class WordFreqMulti{
+public:
+    WordFreqMulti(string& input) : s(input) {
+        transform(s.begin(), s.end(), s.begin(), 
+            [](unsigned char c) -> unsigned char { return std::tolower(c); });
+        stringstream ss(s);
+        string word;
+        bookSize = 0;
+        while(getline(ss, word, ' ')){
+            if(!word.empty()){
+                bookSize++;
+                if(wordMap.find(word) == wordMap.end()){
+                    wordMap.insert({word, 0});
+                }
+                wordMap[word]++;
+            }
+        }
+    }
+
+    double compute(string target){
+        if(wordMap.find(target) == wordMap.end())
+            return 0;
+        return wordMap[target] / bookSize;
+    }
+
+private:
+    unordered_map<string, double> wordMap;
+    string s;
+    vector<string> targets;
+    double bookSize;
+};
+
 int main(void){
     string s = "A new door to the unbelievable, to the possible, a new day that can always bring you anything if you have no objection to it.";
     WordFreq wf(s);
     cout << wf.compute("to") << endl;
     cout << wf.compute("a") << endl;
+    WordFreqMulti wfm(s);
+    cout << wfm.compute("to") << endl;
+    cout << wfm.compute("a") << endl;
     return 0;
 }
