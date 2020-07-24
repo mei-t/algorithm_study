df03717b8d0e19c834d8e4fa01c5bbaacf70b926
Mei Takahashi <me.7tspr@gmail.com>
2020-07-24 17:54:23 +0900

    Add 16-12.cpp

---
+++ cracking_the_coding_interview/16-12.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+class XmlParse{
+public:
+    string encodeXml(string& s){
+        int index = 0;
+        string res = "";
+        while(index < s.size()){
+            res += (index == 0 ? "" : " ");
+            if(index > 70)
+                cout << res << endl;
+            switch(s[index]){
+                case '<':
+                    index++;
+                    if(s[index] == '/'){
+                        res += "0";
+                        detectWord(s, &index, '>');
+                    } else {
+                        res += xmlMap[detectWord(s, &index, ' ')];
+                    }
+                    break;
+                case '"':
+                    index += 1;
+                    res += detectWord(s, &index, '"');
+                    break;
+                case ' ':
+                    index++;
+                    res += xmlMap[detectWord(s, &index, '=')];
+                    break;
+                case '>':
+                    res += "0";
+                    break;
+                case '/':
+                    res += "0";
+                    detectWord(s, &index, '>');
+                default:
+                    res += (s[index-1] == ' ' ? xmlMap[detectWord(s, &index, '=')] : detectWord(s, &index, '<'));
+            }
+            index++;
+        }
+        return res;
+    }
+
+private:
+    unordered_map<string, string> xmlMap = {
+        {"family", "1"},
+        {"person", "2"},
+        {"firstName", "3"},
+        {"lastName", "4"},
+        {"state", "5"},
+    };
+
+    string detectWord(string& s, int* index, char del){
+        int start = *index;
+        while(s[*index] != del)
+            (*index)++;
+        // (*index)--;
+        return s.substr(start, *index - start);
+    }
+};
+
+int main(void){
+    string s = "<family lastName=\"McDowell\" state=\"CA\"><person firstName=\"Gayle\">Some Message</person></family>";
+    XmlParse xp;
+    cout << xp.encodeXml(s) << endl;
+    return 0;
+}
