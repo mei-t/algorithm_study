0926bea23f395f67b08a7897299177a7a478ffa1
Mei Takahashi <me.7tspr@gmail.com>
2020-07-25 18:30:02 +0900

    Add basic_calculator_2.cpp.

---
+++ LeetCode/basic_calculator_2.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <queue>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int calculate(string s) {
+        int i = 0;
+        queue<Component> origin;
+        while(i < s.size()){
+            switch(s[i]){
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                    origin.push({Operator, 0, operatorMap[s[i]]});
+                case ' ':
+                    i++;
+                    break;
+                default:
+                    int val = 0;
+                    while(i < s.size() && s[i] >= '0' && s[i] <= '9'){
+                        val *= 10;
+                        val += s[i] - '0';
+                        i++;
+                    }
+                    origin.push({Operand, val, None});
+            }
+        }
+        origin.push({End, 0, None});
+
+        int prev;
+        while(origin.front().type != End){
+            Component current = origin.front();
+            origin.pop();
+            if(current.type == Operand){
+                prev = current.val;
+            } else if(current.ope == Add || current.ope == Subt){
+                origin.push({Operand, prev, None});
+                origin.push(current);
+            } else {
+                Component next = origin.front();
+                origin.pop();
+                prev = caluculate(prev, next.val, current.ope);
+            }
+        }
+        origin.push({Operand, prev, None});
+        origin.pop();
+
+        int ans = 0;
+        while(!origin.empty()){
+            Component current = origin.front();
+            origin.pop();
+            if(current.type == Operand){
+                ans += current.val;
+            }else{
+                Component next = origin.front();
+                origin.pop();
+                ans = caluculate(ans, next.val, current.ope);
+            }
+        }
+        return ans;
+    }
+
+private:
+    enum Type { Operand, Operator, End};
+    enum Ope { Add, Subt, Mult, Divi, None};
+
+    struct Component{
+        Type type;
+        int val;
+        Ope ope;
+    };
+
+    unordered_map<char, Ope> operatorMap = {
+        {'+', Add},
+        {'-', Subt},
+        {'*', Mult},
+        {'/', Divi}
+    };
+
+    int caluculate(int x, int y, Ope ope){
+        switch(ope){
+            case Add:
+                return x + y;
+            case Subt:
+                return x - y;
+            case Mult:
+                return x * y;
+            case Divi:
+                return x / y;
+            default:
+                return 0;
+        }
+    }
+};
+
+int main(void){
+    string s = "3+2*2";
+    Solution sol;
+    cout << sol.calculate(s) << endl;
+}
