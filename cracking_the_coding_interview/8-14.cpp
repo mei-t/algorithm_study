4f5bdbd71b3d37d0cdec5bd4eb87751deb9b1e73
Mei Takahashi <me.7tspr@gmail.com>
2020-07-30 14:06:42 +0900

    Modify

--- cracking_the_coding_interview/8-14.cpp
+++ cracking_the_coding_interview/8-14.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unordered_map>
 using namespace std;
 
 // int countEval(string formula, bool res, int index){
@@ -31,12 +32,13 @@ using namespace std;
 //     return countEval(formula, res, 1);
 // }
 
-int countEval(string formula, bool res){
+int countEval(string formula, bool res, unordered_map<string, int>& memo){
     if(formula.size() == 0)
         return 0;
-    if(formula.size() == 1){
-        cout << formula << ", res = " << res << "toi(formula) == res ? " << (stoi(formula) == res) << endl;
+    if(formula.size() == 1)
         return stoi(formula) == res ? 1 : 0;
+    if(memo.find(formula + to_string(res)) != memo.end()){
+        return memo[formula + to_string(res)];
     }
     
     int ways = 0;
@@ -45,10 +47,10 @@ int countEval(string formula, bool res){
         string left = formula.substr(0, i);
         string right = formula.substr(i + 1);
 
-        int leftTrue = countEval(left, true);
-        int leftFalse = countEval(left, false);
-        int rightTrue = countEval(right, true);
-        int rightFalse = countEval(right, false);
+        int leftTrue = countEval(left, true, memo);
+        int leftFalse = countEval(left, false, memo);
+        int rightTrue = countEval(right, true, memo);
+        int rightFalse = countEval(right, false, memo);
         int total = (leftTrue + leftFalse) * (rightTrue + rightFalse);
 
         int totalTrue = 0;
@@ -60,18 +62,23 @@ int countEval(string formula, bool res){
                 totalTrue = leftTrue * rightFalse + leftFalse * rightTrue;
                 break;
             case '|':
-                // totalTrue = leftTrue * rightTrue + leftTrue * rightFalse + leftFalse * rightTrue;
                 totalTrue = total - leftFalse * rightFalse;
         }
         ways += res ? totalTrue : total - totalTrue;
     }
+    memo.insert({formula + to_string(res), ways});
     return ways;
+}
+
+int countEval(string formula, bool res){
+    unordered_map<string, int> memo;
+    return countEval(formula, res, memo);
 }
 
 int main(void){
     string input1 = "1^0|0|1";
     string input2 = "0&0&0&1^1|0";
-    // cout << countEval(input1, false) << endl;
+    cout << countEval(input1, false) << endl;
     cout << countEval(input2, true) << endl;
     return 0;
 }
