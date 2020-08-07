aa02f42a9d6377f789d9a5c73b563499608546c9
Mei Takahashi <me.7tspr@gmail.com>
2020-08-07 12:35:52 +0900

    Add 16-24.cpp

---
+++ cracking_the_coding_interview/16-24.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+vector<pair<int, int>> findSum(const vector<int>& nums, int target){
+    unordered_set<int> numSet;
+    unordered_set<int> done;
+    vector<pair<int, int>> res;
+    for(int num: nums){
+        if(numSet.count(num) == 0){
+            numSet.insert({target - num});
+            continue;
+        }
+        if(done.count(num) == 0){
+            res.push_back(pair<int, int>(num, target - num));
+            done.insert(num);
+            done.insert(target - num);
+        }
+    }
+    return res;
+}
+
+int main(void){
+    vector<int> input1 = {1, 2, 3, 4, 5};
+    vector<pair<int, int>> res1 = findSum(input1, 5);
+    for(auto numPair: res1){
+        cout << numPair.first << " " << numPair.second << endl;
+    }
+    return 0;
+}
