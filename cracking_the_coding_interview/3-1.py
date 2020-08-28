5d52a9c7f3d535f94f5b208f6ac3ebac8542284a
Mei Takahashi <me.7tspr@gmail.com>
2020-08-28 22:01:25 +0900

    Fix and add test

--- cracking_the_coding_interview/3-1.py
+++ cracking_the_coding_interview/3-1.py
@@ -5,7 +5,7 @@ class ThreeStacks:
         self.list = [None for _ in range(size)]
         self.size = size
         self.fir_i = 0
-        self.sec_i = size * 2 / 3
+        self.sec_i = (size * 2 - 1) // 3
         self.thi_i = size - 1
     
     def pushFirst(self, val):
@@ -25,7 +25,7 @@ class ThreeStacks:
         return True
     
     def pushThird(self, val):
-        if self.thi_i <= self.size * 2 / 3:
+        if self.thi_i <= (self.size * 2 - 1) // 3:
             return False
         
         self.list[self.thi_i] = val
@@ -38,20 +38,49 @@ class ThreeStacks:
         
         res = self.list[self.fir_i - 1]
         self.list[self.fir_i - 1] = None
+        self.fir_i -= 1
         return res
     
     def popSecond(self):
-        if self.sec_i == self.size * 2 / 3:
+        if self.sec_i == (self.size * 2 - 1) // 3:
             return None
         
         res = self.list[self.sec_i + 1]
         self.list[self.sec_i + 1] = None
+        self.sec_i += 1
         return res
     
     def popThird(self):
-        if self.thi_i == self.size:
+        if self.thi_i == self.size - 1:
             return None
         
         res = self.list[self.thi_i + 1]
         self.list[self.thi_i + 1] = None
+        self.thi_i += 1
         return res
+
+
+class Test(unittest.TestCase):
+    def test_first_second_simple(self):
+        ts = ThreeStacks(6)
+        self.assertTrue(ts.pushFirst(0))
+        self.assertTrue(ts.pushFirst(1))
+        self.assertTrue(ts.pushSecond(0))
+        self.assertTrue(ts.pushSecond(1))
+        self.assertFalse(ts.pushFirst(2))
+        self.assertTrue(ts.pushThird(0))
+        self.assertTrue(ts.pushThird(1))
+        self.assertFalse(ts.pushThird(2))
+        
+        self.assertEqual(ts.popFirst(), 1)
+        self.assertEqual(ts.popFirst(), 0)
+        self.assertEqual(ts.popFirst(), None)
+        self.assertEqual(ts.popSecond(), 1)
+        self.assertEqual(ts.popSecond(), 0)
+        self.assertEqual(ts.popSecond(), None)
+        self.assertEqual(ts.popThird(), 1)
+        self.assertEqual(ts.popThird(), 0)
+        self.assertEqual(ts.popThird(), None)
+
+if __name__ == '__main__':
+    unittest.main()
