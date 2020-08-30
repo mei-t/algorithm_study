1930b2a60ca5d14efd290d4a7978d851fddaab69
Mei Takahashi <me.7tspr@gmail.com>
2020-08-30 19:04:47 +0900

    Modify

--- cracking_the_coding_interview/4-1.py
+++ cracking_the_coding_interview/4-1.py
@@ -3,28 +3,18 @@ class Node:
         self.val = val
         self.nexts = nexts
 
-def find_root(start, end, cur_node):
+def find_root(start, end, cur_node, node_set):
     if cur_node == end:
         return True
-    if not cur_node or cur_node == start:
+    if not cur_node or cur_node in node_set:
         return False
 
+    node_set.add(node_set)
     for node in cur_node:
-        if find_root(start, end, node):
+        if find_root(start, end, node, node_set):
             return True
 
     return False
 
 def is_root(node1, node2):
-    if node1 == node2:
-        return True
-
-    for node in node1.next:
-        if find_root(node1, node2, node):
-            return True
-
-    for node in node2.next:
-        if find_root(node2, node1, node):
-            return True
-
-    return False
+    return find_root(node1, node2, node1, set()) or find_root(node2, node1, node2, set())
