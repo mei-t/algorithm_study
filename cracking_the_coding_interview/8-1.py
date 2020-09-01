1596a717545ffd770195d53568dab568d5f8aba3
Mei Takahashi <me.7tspr@gmail.com>
2020-09-01 22:02:55 +0900

    Change to better solution

--- cracking_the_coding_interview/8-1.py
+++ cracking_the_coding_interview/8-1.py
@@ -1,9 +1,15 @@
-def triple_step(k):
+def count_ways(k, map):
+    if k in map:
+        return map[k]
     if k < 0:
         return 0
     if k == 0:
         return 1
-    return triple_step(k - 1) + triple_step(k - 2) + triple_step(k - 3)
+    map[k] = triple_step(k - 1) + triple_step(k - 2) + triple_step(k - 3)
+    return map[k]
+
+def triple_step(k):
+    return count_ways(k, dict())
 
 if __name__ == '__main__':
-    print(triple_step(6))
+    print(triple_step(3))
