3b3efd5444f9657f65388b539a18c140d7fdda02
mei-t <me.7tspr@gmail.com>
2020-10-29 16:46:54 +0900

    Add sum_of_two_integers.py.

---
+++ LeetCode/sum_of_two_integers.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def getSum(self, a, b):
+        # if not (a & (1 << 31)) ^ (b & (1 << 31)):
+        res = 0
+        carry = 0
+        for i in range(31):
+            mask = 1 << i
+            a_masked = mask & a
+            b_masked = mask & b
+            if a_masked ^ b_masked ^ carry:
+                res |= mask
+            carry = (a_masked & b_masked) | (b_masked & carry) | (carry & a_masked)
+            
+        return res
+
+if __name__ == '__main__':
+    sol = Solution()
+    res = sol.getSum(2, 3)
+    print(res)
