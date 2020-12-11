3bfb24bcd7a53b9663b5b449c9827f6ccefe243b
mei-t <me.7tspr@gmail.com>
2020-12-11 15:20:56 +0900

    Modify

--- LeetCode/subsets.py
+++ LeetCode/subsets.py
@@ -3,19 +3,11 @@ import copy
 class Solution(object):
     def subsets(self, nums):
         res = [[]]
-        self.addSubsets(nums, 0, res)
+        
+        for num in nums:
+            res += [cur + [num] for cur in res]
+        
         return res
-
-    def addSubsets(self, nums, i, res):
-        if i >= len(nums):
-            return
-
-        for j in range(len(res)):
-            newSubset = copy.deepcopy(res[j])
-            newSubset.append(nums[i])
-            res.append(newSubset)
-
-        self.addSubsets(nums, i+1, res)
 
 if __name__ == '__main__':
     nums = [1,2,3]
