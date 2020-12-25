082e100f496bdd2c35c23187763a658e68211e24
mei-t <me.7tspr@gmail.com>
2020-12-25 15:26:47 +0900

    Add another solution.

--- LeetCode/spiral_matrix.py
+++ LeetCode/spiral_matrix.py
@@ -1,4 +1,4 @@
-class Solution(object):
+class Solution1(object):
     def spiralOrder(self, matrix):
         if len(matrix) == 0:
             return []
@@ -11,7 +11,7 @@ class Solution(object):
         while True:
             if left == right:
                 break
-            
+
             for cur_j in range(left, right):
                 res.append(matrix[cur_i][cur_j])
             right -= 1
@@ -39,8 +39,36 @@ class Solution(object):
         
         return res
 
+class Solution2(object):
+    def spiralOrder(self, matrix):
+        if not matrix:
+            return []
+
+        R = len(matrix)
+        C = len(matrix[0])
+        is_visited = [[False for _ in range(C)] for _ in range(R)]
+        r_next = [0, 1, 0, -1]
+        c_next = [1, 0, -1, 0]
+        d = 0
+        res = []
+        r = c = 0
+        for _ in range(R * C):
+            res.append(matrix[r][c])
+            is_visited[r][c] = True
+
+            r_tmp = r + r_next[d]
+            c_tmp = c + c_next[d]
+            if 0 <= r_tmp < R and 0 <= c_tmp < C and not is_visited[r_tmp][c_tmp]:
+                r, c = r_tmp, c_tmp
+            else:
+                d = (d + 1) % 4
+                r, c = r + r_next[d], c + c_next[d]
+        
+        return res
+
+
 if __name__ == '__main__':
-    sol = Solution()
+    sol = Solution2()
     matrix1 = [[1,2,3],[4,5,6],[7,8,9]]
     matrix2 = [[1, 2, 3, 4],[5, 6, 7, 8],[9, 10, 11, 12]]
     res1 = sol.spiralOrder(matrix1)
