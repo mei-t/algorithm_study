5a0a22f9bd6739c3f0370734f25831cfad7461f8
mei-t <me.7tspr@gmail.com>
2021-01-01 19:22:52 +0900

    Modify add1 and add another aproach and test.

--- cracking_the_coding_interview/17-1.py
+++ cracking_the_coding_interview/17-1.py
@@ -1,8 +1,10 @@
-def add(num1, num2):
+import unittest
+
+def add1(num1, num2):
     carry = 0
     i = 1
     res = 0
-    while num1 > 0 or num2 > 0:
+    while num1 > 0 or num2 > 0 or carry:
         b1 = num1 & 1
         b2 = num2 & 1
         res += (b1 ^ b2 ^ carry) * i
@@ -12,6 +14,23 @@ def add(num1, num2):
         i = i << 1
     return res
 
+def add2(num1, num2):
+    if num2 == 0:
+        return num1
+
+    res = num1 ^ num2
+    carry = (num1 & num2) << 1
+    return add2(res, carry)
+
+class Test(unittest.TestCase):
+
+    def test_add1(self):
+        self.assertEqual(add1(2, 5), 7)
+        self.assertEqual(add1(13, 29), 42)
+
+    def test_add2(self):
+        self.assertEqual(add2(2, 5), 7)
+        self.assertEqual(add2(13, 29), 42)
+
 if __name__ == "__main__":
-    print(add(2, 5))
-    print(add(13, 29))
+    unittest.main()
