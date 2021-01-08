770fe1b8da661460e66593fd1daab56393e97a28
mei-t <me.7tspr@gmail.com>
2021-01-08 15:01:10 +0900

    Add another solution.

--- LeetCode/letter_combinations_of_a_phone_number.py
+++ LeetCode/letter_combinations_of_a_phone_number.py
@@ -1,4 +1,4 @@
-class Solution(object):
+class Solution1(object):
     char_map = {
         "2": ["a", "b", "c"],
         "3": ["d", "e", "f"],
@@ -25,8 +25,36 @@ class Solution(object):
         for c in self.char_map[digits[i]]:
             self.nextLetter(digits, i+1, s+c, res)
 
+class Solution2(object):
+    char_map = {
+        "2": ["a", "b", "c"],
+        "3": ["d", "e", "f"],
+        "4": ["g", "h", "i"],
+        "5": ["j", "k", "l"],
+        "6": ["m", "n", "o"],
+        "7": ["p", "q", "r", "s"],
+        "8": ["t", "u", "v"],
+        "9": ["w", "x", "y", "z"]
+    }
+
+    def letterCombinations(self, digits):
+        if len(digits) == 0:
+            return []
+        return self.nextLetter(digits, len(digits) - 1, [""])
+    
+    def nextLetter(self, digits, i, later):
+        cur = []
+        for s in later:
+            for c in self.char_map[digits[i]]:
+                cur.append(c + s)
+
+        if i == 0:
+            return cur
+        
+        return self.nextLetter(digits, i-1, cur)
+
 if __name__ == '__main__':
-    sol = Solution()
+    sol = Solution1()
     digits = "23"
     res = sol.letterCombinations(digits)
     print(res)
