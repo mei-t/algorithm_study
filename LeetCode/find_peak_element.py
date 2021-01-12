b3fdf9e4b2c06bdb37676bbf443445f81f954790
mei-t <me.7tspr@gmail.com>
2021-01-12 18:25:53 +0900

    Add another solution.

--- LeetCode/find_peak_element.py
+++ LeetCode/find_peak_element.py
@@ -1,19 +1,24 @@
-class Solution(object):
+class Solution1(object):
     def findPeakElement(self, nums):
-        if len(nums) == 0:
-            return None
-        if len(nums) == 1:
-            return 0
-        if nums[0] > nums[1]:
-            return 0
-        if nums[len(nums) - 1] > nums[len(nums) - 2]:
-            return len(nums) - 1
-
-        for i in range(1, len(nums) - 1):
-            if nums[i] > nums[i - 1] and nums[i] > nums[i + 1]:
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
                 return i
+        return len(nums) - 1
+
+class Solution2(object):
+    def findPeakElement(self, nums):
+        return self.binarySearch(0, len(nums) - 1, nums)
+    
+    def binarySearch(self, l, r, nums):
+        if l == r:
+            return l
+
+        mid = (l + r) // 2
+        if nums[mid] > nums[mid + 1]:
+            return self.binarySearch(l, mid, nums)
+        return self.binarySearch(mid + 1, r, nums)
 
 if __name__ == "__main__":
     nums = [1, 2, 1, 3, 5, 6, 4]
-    sol = Solution()
+    sol = Solution2()
     print(sol.findPeakElement(nums))
