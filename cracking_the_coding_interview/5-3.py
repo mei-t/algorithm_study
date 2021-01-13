0694181f318efbc01fbed71e5e7aa9b92465e24a
mei-t <me.7tspr@gmail.com>
2021-01-13 13:37:08 +0900

    Add another solution

--- cracking_the_coding_interview/5-3.py
+++ cracking_the_coding_interview/5-3.py
@@ -1,7 +1,7 @@
 import unittest
 
 # 0は含まないと仮定
-def inversionPos(num):
+def inversionPos1(num):
     bit_list = [0]
     i = 0
     is_one = True
@@ -22,10 +22,26 @@ def inversionPos(num):
         next_one = bit_list[i + 2] if bit_list[i + 1] == 1 else 0
         l = bit_list[i] + next_one + 1
         max_l = max(max_l, l)
-        
+
     max_l = max(max_l, bit_list[-1] + 1)
     
     return max_l
+
+def inversionPos(num):
+    prev_len = 0
+    cur_len = 0
+    max_len = 0
+    while num:
+        if num & 1:
+            cur_len += 1
+        else:
+            prev_len = cur_len if num & 2 else 0
+            cur_len = 0
+        
+        max_len = max(max_len, cur_len + prev_len + 1)
+        num >>= 1
+    
+    return max_len
 
 class Test(unittest.TestCase):
     def test_simple(self):
@@ -34,9 +50,9 @@ class Test(unittest.TestCase):
         self.assertEqual(inversionPos(0b1001100111), 4)
     
     def test_edge_case(self):
-        self.assertEqual(inversionPos(1), 1)
+        self.assertEqual(inversionPos(1), 2)
         self.assertEqual(inversionPos(2), 2)
-        self.assertEqual(inversionPos(3), 2)
+        self.assertEqual(inversionPos(3), 3)
         self.assertEqual(inversionPos(4), 2)
         self.assertEqual(inversionPos(5), 3)
 
