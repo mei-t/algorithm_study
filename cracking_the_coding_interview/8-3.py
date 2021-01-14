44359f7eeba6c9fae7a86861747c93ab82714118
mei-t <me.7tspr@gmail.com>
2021-01-14 12:47:35 +0900

    Rename bts_magic_index to bs_magic_index.

--- cracking_the_coding_interview/8-3.py
+++ cracking_the_coding_interview/8-3.py
@@ -1,17 +1,17 @@
 import unittest
 
 def magic_index(nums):
-    return bts_magic_index(nums, 0, len(nums))
+    return bs_magic_index(nums, 0, len(nums))
 
-def bts_magic_index(nums, l, r):
+def bs_magic_index(nums, l, r):
     if l == r:
         return l if nums[l] == l else NotImplemented
     
     mid = (l + r) // 2
     if nums[mid] < mid:
-        return bts_magic_index(nums, mid + 1, r)
+        return bs_magic_index(nums, mid + 1, r)
     elif nums[mid] > mid:
-        return bts_magic_index(nums, l, mid)
+        return bs_magic_index(nums, l, mid)
     else:
         return mid
 
