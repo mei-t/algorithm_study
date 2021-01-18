6b8f7572a73f4817d93357f24c4c572418011014
mei-t <me.7tspr@gmail.com>
2021-01-18 19:10:29 +0900

    Add another solution

--- cracking_the_coding_interview/1-4.py
+++ cracking_the_coding_interview/1-4.py
@@ -15,17 +15,36 @@ def is_palindrome(s):
                 return False
     return True
 
+def is_palindrome_with_bit_vector(s):
+    bit_vector = 0
+    for c in s.lower().replace(" ", ""):
+        x = 1 << (ord(c) - ord("a"))
+        bit_vector ^= x
+
+    if bit_vector == 0:
+        return True
+
+    odd_appear = False
+    for i in range(26):
+        x = 1 << i
+        if bit_vector & x:
+            if odd_appear:
+                return False
+            odd_appear = True
+
+    return True
+
 class Test(unittest.TestCase):
     t_data = ["Tact Coa", "a", "", "ksjesssjeek"]
     f_data = ["Tact Co", "ab", "sjfueska"]
 
     def test_true(self):
         for data in self.t_data:
-            self.assertTrue(is_palindrome(data))
+            self.assertTrue(is_palindrome_with_bit_vector(data))
     
     def test_false(self):
         for data in self.f_data:
-            self.assertFalse(is_palindrome(data))
+            self.assertFalse(is_palindrome_with_bit_vector(data))
 
 if __name__ == '__main__':
     unittest.main()
