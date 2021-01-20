7ee356c27270a8602ef4b4cf17dd79d85f4669ca
mei-t <me.7tspr@gmail.com>
2021-01-20 23:25:29 +0900

    Add another solution

--- LeetCode/three_sum.py
+++ LeetCode/three_sum.py
@@ -1,4 +1,4 @@
-class Solution(object):
+class Solution1(object):
     def threeSum(self, nums):
         if len(nums) < 3:
             return []
@@ -57,6 +57,31 @@ class Solution(object):
                     nums.append(num)
                     res.append(nums)
                 num_map.pop(num)
+
+class Solution(object):
+    def threeSum(self, nums):
+        nums.sort()
+        res = []
+
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s > 0:
+                    r -= 1
+                elif s < 0:
+                    l += 1
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                    while l < r and nums[l] == nums[l + 1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r - 1]:
+                        r -= 1
+                    l += 1
+                    r -= 1
+        return res
 
 if __name__ == "__main__":
     nums = [-1, 0, 1, 2, -1, -4]
