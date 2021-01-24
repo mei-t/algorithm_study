7c75d3642ec8494d35328dc28c3f53d7c4a0b313
mei-t <me.7tspr@gmail.com>
2021-01-24 18:29:08 +0900

    Add 10-4.py

---
+++ cracking_the_coding_interview/10-4.py
@@ -0,0 +1,59 @@
+class Array:
+    def __init__(self, array=[]):
+        self.array = sorted(array)
+    
+    def element_at(self, i):
+        if not self.array or len(self.array) <= i:
+            return -1
+        return self.array[i]
+
+def find_index(nums, x):
+    if nums.element_at(0) == -1:
+        return None
+
+    if nums.element_at(0) == x:
+        return 0
+    
+    l, r = find_range(nums, x)
+    return binary_search(nums, l, r, x)
+
+def find_range(nums, target):
+    i = 1
+    l = i
+    while nums.element_at(i) >= 0 and nums.element_at(i) < target:
+        l = i
+        i *= 2
+    return l, binary_search_range(nums, l, i, target)
+
+def binary_search_range(nums, left, right, target):
+    if left > right:
+        return right
+    mid = (left + right) // 2
+    if nums.element_at(mid) >= 0 and nums.element_at(mid + 1) == -1:
+        return mid + 1
+
+    if nums.element_at(mid) > target:
+        return mid
+    
+    if nums.element_at(mid) == -1:
+        return binary_search_range(nums, left, mid, target)
+    else:
+        return binary_search_range(nums, mid + 1, right, target)
+
+def binary_search(nums, left, right, target):
+    if left > right:
+        return None
+    
+    mid = (left + right) // 2
+    if nums.element_at(mid) == target:
+        return mid
+    elif nums.element_at(mid) > target:
+        return binary_search(nums, left, mid, target)
+    else:
+        return binary_search(nums, mid + 1, right, target)
+
+if __name__ == "__main__":
+    nums = [0, 1, 2, 3, 4, 5]
+    array = Array(nums)
+    res = find_index(array, 6)
+    print(res)
