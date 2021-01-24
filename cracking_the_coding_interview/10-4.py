1d5e5088d7dce903a9c276b31d80270b5ccd3f6d
mei-t <me.7tspr@gmail.com>
2021-01-24 19:51:55 +0900

    Modify

--- cracking_the_coding_interview/10-4.py
+++ cracking_the_coding_interview/10-4.py
@@ -24,35 +24,23 @@ def find_index(nums, x):
 
 def find_range(nums, target):
     i = 1
-    l = i
     while nums.element_at(i) >= 0 and nums.element_at(i) < target:
-        l = i
         i *= 2
-    return l, binary_search_range(nums, l, i, target)
-
-def binary_search_range(nums, left, right, target):
-    if left > right:
-        return right
-    mid = (left + right) // 2
-    if nums.element_at(mid) >= 0 and nums.element_at(mid + 1) == -1:
-        return mid + 1
-
-    if nums.element_at(mid) > target:
-        return mid
-    
-    if nums.element_at(mid) == -1:
-        return binary_search_range(nums, left, mid, target)
-    else:
-        return binary_search_range(nums, mid + 1, right, target)
+    return i // 2, i
 
 def binary_search(nums, left, right, target):
     if left > right:
         return None
     
+    if left == right:
+        if nums.element_at(left) == -1:
+            return None
+        return left
+    
     mid = (left + right) // 2
     if nums.element_at(mid) == target:
         return mid
-    elif nums.element_at(mid) > target:
+    elif nums.element_at(mid) > target or nums.element_at(mid) == -1:
         return binary_search(nums, left, mid, target)
     else:
         return binary_search(nums, mid + 1, right, target)
