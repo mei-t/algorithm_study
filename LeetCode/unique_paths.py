576310080e25c6738dff918619348d86b32b3d6e
mei-t <me.7tspr@gmail.com>
2021-01-26 02:32:31 +0900

    Add another solution

--- LeetCode/unique_paths.py
+++ LeetCode/unique_paths.py
@@ -1,5 +1,15 @@
 class Solution(object):
     def uniquePaths(self, m, n):
+        m -= 1
+        n -= 1
+        nume, deno = 1, 1
+        for i in range(min(m, n)):
+            nume = nume * (m + n - i)
+            deno = deno * (min(m, n) - i)
+        return int(nume / deno)
+
+class Solution1(object):
+    def uniquePaths(self, m, n):
         if m == 1 or n == 1:
             return 1
         nume = self.factorial(m + n - 2, max(m, n))
@@ -13,5 +23,5 @@ class Solution(object):
 
 if __name__ == "__main__":
     sol = Solution()
-    res = sol.uniquePaths(1, 10)
+    res = sol.uniquePaths(3, 7)
     print(res)
