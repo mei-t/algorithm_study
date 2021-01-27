a2e1555a495e6ffd1ee544b7d0efa4907fb31605
mei-t <me.7tspr@gmail.com>
2021-01-27 18:38:28 +0900

    Add test

--- LeetCode/string_to_integer.py
+++ LeetCode/string_to_integer.py
@@ -1,3 +1,5 @@
+import unittest
+
 class Solution(object):
     max_num = 2147483647
     min_num = -2147483648
@@ -35,11 +37,31 @@ class Solution(object):
         
         return ans
 
+class Test(unittest.TestCase):
+    def test_simple(self):
+        sol = Solution()
+        res = sol.myAtoi("42")
+        self.assertEqual(res, 42)
+    
+    def test_with_white_space(self):
+        sol = Solution()
+        res = sol.myAtoi("   -42")
+        self.assertEqual(res, -42)
+    
+    def test_with_post_words(self):
+        sol = Solution()
+        res = sol.myAtoi("4193 with word")
+        self.assertEqual(res, 4193)
+    
+    def test_with_pre_words(self):
+        sol = Solution()
+        res = sol.myAtoi("words and 987")
+        self.assertEqual(res, 0)
+    
+    def test_overflow(self):
+        sol = Solution()
+        res = sol.myAtoi("-91283472332")
+        self.assertEqual(res, -2147483648)
+
 if __name__ == '__main__':
-    sol = Solution()
-    # res = sol.myAtoi("42")
-    # res = sol.myAtoi("   -42")
-    # res = sol.myAtoi("4193 with words")
-    # res = sol.myAtoi("words and 987")
-    res = sol.myAtoi("-91283472332")
-    print(res)
+    unittest.main()
