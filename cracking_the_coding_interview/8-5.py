eba16a0c0ea26f4af0210931de1fcc7521c8ce97
mei-t <me.7tspr@gmail.com>
2021-01-31 11:15:31 +0900

    Add test

--- cracking_the_coding_interview/8-5.py
+++ cracking_the_coding_interview/8-5.py
@@ -1,3 +1,5 @@
+import unittest
+
 def multiplication(a, b):
     def compute(larger, smaller):
         if smaller == 0:
@@ -13,6 +15,11 @@ def multiplication(a, b):
 
     return compute(max(a, b), min(a, b))
 
+class Test(unittest.TestCase):
+    def test_simple(self):
+        self.assertEqual(multiplication(3, 5), 15)
+        self.assertEqual(multiplication(20, 34), 680)
+        self.assertEqual(multiplication(0, 2), 0)
+
 if __name__ == '__main__':
-    res = multiplication(20, 34)
-    print(res)
+    unittest.main()
