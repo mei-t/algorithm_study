5dfb4c53de13bdedc77215095cd64750597aa6ec
mei-t <me.7tspr@gmail.com>
2021-02-02 08:44:49 +0900

    Add another solution

--- LeetCode/meeting_rooms_2.py
+++ LeetCode/meeting_rooms_2.py
@@ -1,6 +1,6 @@
 import heapq
 
-class Solution(object):
+class Solution1(object):
     def minMeetingRooms(self, intervals):
         intervals.sort()
         q = []
@@ -13,6 +13,32 @@ class Solution(object):
                 count = len(q)
         
         return count
+
+class Solution2(object):
+    def minMeetingRooms(self, intervals):
+        intervals.sort()
+        q = []
+        for interval in intervals:
+            while q and q[0] != 0 and q[0] <= interval[0]:
+                heapq.heappop(q)
+                heapq.heappush(q, 0)
+            if q and q[0] == 0:
+                heapq.heappop(q)
+            heapq.heappush(q, interval[1])
+        
+        return len(q)
+
+class Solution(object):
+    def minMeetingRooms(self, intervals):
+        intervals.sort()
+        q = []
+        heapq.heappush(q, intervals[0][1])
+        for interval in intervals[1:]:
+            if q[0] <= interval[0]:
+                heapq.heappop(q)
+            heapq.heappush(q, interval[1])
+        
+        return len(q)
 
 if __name__ == '__main__':
     intervals = [[0, 30], [5, 10], [15, 20]]
