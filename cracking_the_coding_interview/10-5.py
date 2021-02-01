0d342ef6f0932a628824f05bf0e80af7feadf56a
mei-t <me.7tspr@gmail.com>
2021-02-01 16:17:32 +0900

    Add test

--- cracking_the_coding_interview/10-5.py
+++ cracking_the_coding_interview/10-5.py
@@ -1,8 +1,10 @@
+import unittest
+
 def find_index(words, target):
     return search(words, target, 0, len(words))
 
 def search(words, target, left, right):
-    if left > right:
+    if left >= right:
         return -1
     
     mid = (left + right) // 2
@@ -21,7 +23,14 @@ def search(words, target, left, right):
     
     return search(words, target, mid + 1, right)
 
+class Test(unittest.TestCase):
+    def test_simple(self):
+        words = ["at", "", "", "", "ball", "", "", "car", "", "", "dad", "", ""]
+        self.assertEqual(find_index(words, "at"), 0)
+        self.assertEqual(find_index(words, "ball"), 4)
+        self.assertEqual(find_index(words, "car"), 7)
+        self.assertEqual(find_index(words, "dad"), 10)
+        self.assertEqual(find_index(words, "bake"), -1)
+
 if __name__ == '__main__':
-    words = ["at", "", "", "", "ball", "", "", "car", "", "", "dad", "", ""]
-    res = find_index(words, "ball")
-    print(res)
+    unittest.main()
