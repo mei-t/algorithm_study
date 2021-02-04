2be0b174c0135d56961c40729cf9c73ec5403ba3
mei-t <me.7tspr@gmail.com>
2021-02-04 22:21:32 +0900

    Modify

--- LeetCode/flatten_2d_vector.py
+++ LeetCode/flatten_2d_vector.py
@@ -1,5 +1,30 @@
-# 27:47
+# 27:47 問題勘違いしてた
+
 class Vector2D(object):
+
+    def __init__(self, v):
+        self.v = v
+        self.inner = 0
+        self.outer = 0
+    
+    def move_next(self):
+        while self.outer < len(self.v) and self.inner == len(self.v[self.outer]):
+            self.outer += 1
+            self.inner = 0
+
+    def next(self):
+        self.move_next()
+        res = self.v[self.outer][self.inner]
+        self.inner += 1
+        return res
+        
+
+    def hasNext(self):
+        self.move_next()
+        return self.outer < len(self.v)
+
+# 勘違いしてた
+class Vector2D1(object):
 
     def __init__(self, v):
         def flatten(nums):
@@ -25,7 +50,7 @@ class Vector2D(object):
         return self.pos < len(self.res)
 
 if __name__ == '__main__':
-    v = [[[[1,2],[3],[4]]],[None],[None],[None],[None],[None],[None],[None]]
+    v = [[1,2],[3],[4]]
     obj = Vector2D(v)
     print(obj.next())
     print(obj.next())
