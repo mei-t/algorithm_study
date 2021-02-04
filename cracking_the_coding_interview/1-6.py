1ebf1ab7758aa7030b6fc067bc80164853e077b6
mei-t <me.7tspr@gmail.com>
2021-02-04 12:20:55 +0900

    Add test

--- cracking_the_coding_interview/1-6.py
+++ cracking_the_coding_interview/1-6.py
@@ -1,3 +1,5 @@
+import unittest
+
 def compress_string(s):
     prev = s[0]
     count = 1
@@ -11,7 +13,17 @@ def compress_string(s):
     compressed += prev + str(count)
     return compressed if len(compressed) < len(s) else s
 
+class Test(unittest.TestCase):
+    data_compressed = [["aabcccccaaa", "a2b1c5a3"], ["aaa", "a3"]]
+    data_not_compressed = [["a", "a"], ["aa", "aa"], ["abcdef", "abcdef"]]
+
+    def test_compressed(self):
+        for d in self.data_compressed:
+            self.assertEqual(compress_string(d[0]), d[1])
+
+    def test_not_compressed(self):
+        for d in self.data_not_compressed:
+            self.assertEqual(compress_string(d[0]), d[1])
+
 if __name__ == '__main__':
-    s = "aabcccccaaa"
-    res = compress_string(s)
-    print(res)
+    unittest.main()
