37541a2ce27d737b8e3499ca92f78e5a2ecb1dd9
mei-t <me.7tspr@gmail.com>
2021-02-10 14:51:41 +0900

    Add better solution

--- LeetCode/design_tic-tac-toe.py
+++ LeetCode/design_tic-tac-toe.py
@@ -1,5 +1,31 @@
 # 13:40
+
+# TC: O(1)
 class TicTacToe(object):
+
+    def __init__(self, n):
+        self.rows = [0 for _ in range(n)]
+        self.cols = [0 for _ in range(n)]
+        self.diagonal = [0, 0]
+        self.n = n
+        
+
+    def move(self, row, col, player):
+        offset = 2 * player - 3
+        self.rows[row] += offset
+        self.cols[col] += offset
+        if row == col:
+            self.diagonal[0] += offset
+        if row == self.n - col - 1:
+            self.diagonal[1] += offset
+        if self.rows[row] == self.n or self.cols[col] == self.n or self.n in self.diagonal:
+            return 2
+        if self.rows[row] == -self.n or self.cols[col] == -self.n or -self.n in self.diagonal:
+            return 1
+        return 0
+
+# TC: O(n)
+class TicTacToe1(object):
 
     def __init__(self, n):
         self.board = [[0 for _ in range(n)] for _ in range(n)]
