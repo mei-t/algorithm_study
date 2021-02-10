7ff2d2ad80f928ea3a6bae23d71c9a7d3ae5de85
mei-t <me.7tspr@gmail.com>
2021-02-10 13:27:29 +0900

    Add test

--- cracking_the_coding_interview/8-6.py
+++ cracking_the_coding_interview/8-6.py
@@ -1,3 +1,6 @@
+import unittest
+from copy import deepcopy
+
 def troy_tower(n, start, tmp, end):
     if n == 0:
         return
@@ -5,9 +8,13 @@ def troy_tower(n, start, tmp, end):
     end.append(start.pop())
     troy_tower(n - 1, tmp, start, end)
 
+class Test(unittest.TestCase):
+    def test_simple(self):
+        start = [6, 5, 4, 3, 2, 1, 0]
+        expected = deepcopy(start)
+        tmp, end = [], []
+        troy_tower(len(start), start, tmp, end)
+        self.assertEqual(end, expected)
 
 if __name__ == '__main__':
-    start = [6, 5, 4, 3, 2, 1, 0]
-    tmp, end = [], []
-    troy_tower(len(start), start, tmp, end)
-    print(end)
+    unittest.main()
