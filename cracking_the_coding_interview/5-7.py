d4c0f6d74e91b3d878dc007b7d411380a2cdb246
mei-t <me.7tspr@gmail.com>
2021-02-23 15:19:35 +0900

    Add test

--- cracking_the_coding_interview/5-7.py
+++ cracking_the_coding_interview/5-7.py
@@ -1,3 +1,5 @@
+import unittest
+
 def exchange_bit_pair(input):
     tmp = input
     even_mask, odd_mask = 0, 0
@@ -9,6 +11,11 @@ def exchange_bit_pair(input):
         tmp >>= 2
     return ((input & even_mask) << 1) | ((input & odd_mask) >> 1)
 
+class Test(unittest.TestCase):
+    def test_simple(self):
+        self.assertEquals(exchange_bit_pair(1), 2)
+        self.assertEquals(exchange_bit_pair(10), 5)
+        self.assertEquals(exchange_bit_pair(int('1101001', 2)), int('10010110', 2))
+
 if __name__ == '__main__':
-    res = exchange_bit_pair(10)
-    print(res)
+    unittest.main()
