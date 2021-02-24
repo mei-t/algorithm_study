a43ce25f591b9c0836925c897ad16eb60d2d17b3
mei-t <me.7tspr@gmail.com>
2021-02-24 13:42:56 +0900

    Add better solution

--- LeetCode/maximal_square.py
+++ LeetCode/maximal_square.py
@@ -1,5 +1,17 @@
 class Solution(object):
     def maximalSquare(self, matrix):
+        dp = [[0 for _ in range(len(matrix[0]) + 1)] for _ in range(len(matrix) + 1)]
+        res = 0
+        for i in range(1, len(matrix) + 1):
+            for j in range(1, len(matrix[0]) + 1):
+                if matrix[i - 1][j - 1] == "1":
+                    dp[i][j] = min(dp[i - 1][j - 1], dp[i][j - 1], dp[i - 1][j]) + 1
+                    res = max(dp[i][j], res)
+                    
+        return res * res
+
+class Solution1(object):
+    def maximalSquare(self, matrix):
         if not matrix:
             return 0
         res = 0
