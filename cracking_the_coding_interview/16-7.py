696cba3f58b6702f476e471ac7996e2b1596623a
mei-t <me.7tspr@gmail.com>
2021-02-28 15:03:12 +0900

    Add test

--- cracking_the_coding_interview/16-7.py
+++ cracking_the_coding_interview/16-7.py
@@ -1,4 +1,5 @@
 import sys
+import unittest
 
 def sign(a):
     return ((a >> (sys.getsizeof(a) - 1)) & 1) ^ 1
@@ -7,6 +8,16 @@ def get_max(a, b):
     k = sign(a - b)
     return a * k + b * (k ^ 1)
 
+class Test(unittest.TestCase):
+    def test_simple(self):
+        self.assertEquals(get_max(16, 3), 16)
+        self.assertEquals(get_max(16, -3), 16)
+        self.assertEquals(get_max(-16, 3), 3)
+        self.assertEquals(get_max(-16, -3), -3)
+    
+    def test_large_num(self):
+        self.assertEquals(get_max(16000000, 3), 16000000)
+        self.assertEquals(get_max(-16000000, 3), 3)
+
 if __name__ == '__main__':
-    res = get_max(-16, -3)
-    print(res)
+    unittest.main()
