dea36cbc7977f3d484f647460bc7c03558c3333d
mei-t <me.7tspr@gmail.com>
2021-02-28 15:38:26 +0900

    Modify

--- LeetCode/queue_reconstruction_by_height.py
+++ LeetCode/queue_reconstruction_by_height.py
@@ -1,8 +1,9 @@
 # 25:49
 class Solution:
     def reconstructQueue(self, people):
-        people.sort(key=lambda x:x[1])
-        people.sort(reverse=True, key=lambda x:x[0])
+        people.sort(key=lambda x:(-x[0],x[1]))
+        # people.sort(key=lambda x:x[1])
+        # people.sort(reverse=True, key=lambda x:x[0])
         res = []
         for person in people:
             res.insert(person[1], person)
