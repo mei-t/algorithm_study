8bf2adbff15ad21da823105f349ade5b7dc55679
mei-t <me.7tspr@gmail.com>
2021-03-01 12:00:32 +0900

    Add test

--- cracking_the_coding_interview/17-6.py
+++ cracking_the_coding_interview/17-6.py
@@ -1,3 +1,5 @@
+import unittest
+
 def count_two(n):
     res = 0
     digit = 1
@@ -9,6 +11,10 @@ def count_two(n):
         n //= 10
     return res
 
+class Test(unittest.TestCase):
+    def test_simple(self):
+        self.assertEquals(count_two(25), 9)
+        self.assertEquals(count_two(223), 71)
+
 if __name__ == '__main__':
-    res = count_two(223)
-    print(res)
+    unittest.main()
