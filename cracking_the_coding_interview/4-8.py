d909cf9a5e461bf26d530ff2b1998b2d8f31f2cf
mei-t <me.7tspr@gmail.com>
2021-03-05 18:26:07 +0900

    Add 4-8.py

---
+++ cracking_the_coding_interview/4-8.py
@@ -0,0 +1,44 @@
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def find_common_parent(root, val1, val2):
+    return search(root, val1, val2)[0]
+
+def search(node, val1, val2):
+    if not node:
+        return None, False
+    
+    if node.val == val1 or node.val == val2:
+        if val1 == val2:
+            return node, True
+        # left_node, is_left = search(node.left, val1, val2)
+        # right_node, is_right = search
+        if search(node.left, val1, val2)[1] or search(node.right, val1, val2)[1]:
+            return node, True
+        return None, True
+    left_node, is_left = search(node.left, val1, val2)
+    right_node, is_right = search(node.right, val1, val2)
+    if is_left and is_right:
+        return node, True
+    if left_node:
+        return left_node, True
+    if right_node:
+        return right_node, True
+    return None, is_left or is_right
+
+def create_tree(nums, i):
+    if len(nums) <= i:
+        return None
+    node = TreeNode(nums[i])
+    node.left = create_tree(nums, i * 2 + 1)
+    node.right = create_tree(nums, i * 2 + 2)
+    return node
+
+if __name__ == '__main__':
+    nums = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    root = create_tree(nums, 0)
+    res = find_common_parent(root, 5, 6)
+    print(res.val)
