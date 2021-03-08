c45274ac26a14f36a8dae7183b187ea1397657cf
mei-t <me.7tspr@gmail.com>
2021-03-08 18:44:35 +0900

    Modify

--- cracking_the_coding_interview/5-8.py
+++ cracking_the_coding_interview/5-8.py
@@ -1,29 +1,29 @@
 def straight_line(screen, width, x1, x2, y):
     line = width * y
 
-    mask = 0
-    for _ in range(8 - (x1 % 8)):
-        mask <<= 1
-        mask += 1
-    screen[line + x1 // 8] |= mask
-
     mask = 0xff
     for i in range((x1 + 8) // 8, x2 // 8):
         screen[line + i] |= mask
     
-    mask = 0
-    for _ in range(x2 % 8):
-        mask <<= 1
-        mask += 1
-    mask <<= (x2 // 8)
-    screen[line + x2 // 8] |= mask
+    l_mask = 0xff
+    l_mask >>= (x1 % 8)
+
+    r_mask = 0xff
+    r_mask <<= (8 - x2 % 8)
+
+    if x1 // 8 == x2 // 8:
+        mask = l_mask & r_mask
+        screen[line + x1 // 8] |= mask
+    else:
+        screen[line + x1 // 8] |= l_mask
+        screen[line + x2 // 8] |= r_mask
 
     return screen
 
 if __name__ == '__main__':
     screen = [0] * 20
     width = 5
-    straight_line(screen, width, 10, 31, 1)
+    straight_line(screen, width, 10, 11, 1)
     for i in range(len(screen) // width):
         for j in range(width):
             byte = screen[i * width + j]
