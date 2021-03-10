b39576fa0c0f4e606dc8a6ff09c78e69d04b82f7
mei-t <me.7tspr@gmail.com>
2021-03-10 21:42:47 +0900

    Add better solution

--- LeetCode/next_permutation.py
+++ LeetCode/next_permutation.py
@@ -30,3 +30,24 @@ class Solution:
         nums[bound], nums[max_index] = nums[max_index], nums[bound]
         self.partial_sort(nums, min_index, bound - 1)
         self.partial_sort(nums, bound + 1, max_index)
+
+class Solution2:
+    def nextPermutation(self, nums):
+        most_left = len(nums) - 1
+        while most_left > 0 and nums[most_left] <= nums[most_left - 1]:
+            most_left -= 1
+        most_left -= 1
+        if most_left < 0:
+            nums.sort()
+            return
+        min_num = float('inf')
+        min_index = -1
+        for i in range(most_left + 1, len(nums)):
+            if nums[i] > nums[most_left] and nums[i] <= min_num:
+                min_num = nums[i]
+                min_index = i
+        nums[min_index], nums[most_left] = nums[most_left], nums[min_index]
+        left = most_left + 1
+        right = len(nums) - 1
+        for i in range((right - left + 1) // 2):
+            nums[left + i], nums[right - i] = nums[right - i], nums[left + i]
