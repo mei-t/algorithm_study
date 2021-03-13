6ad0890c12269271e8a80ce7b181618fc23a5dcf
mei-t <me.7tspr@gmail.com>
2021-03-13 16:00:48 +0900

    Modify

--- AtCoder/handstand.py
+++ AtCoder/handstand.py
@@ -1,7 +1,7 @@
 if __name__ == '__main__':
   n, k = map(int, input().split())
   s = input()
-  nums = []
+  nums = [0]
   prev = s[0]
   count = 0
   for i in range(len(s)):
@@ -12,18 +12,20 @@ if __name__ == '__main__':
       count = 1
       prev = s[i]
   nums.append(count)
+  nums.append(0)
 
-  a = 1 if s[0] == "0" else 0
-  if len(nums) < 2 * k - a:
+  a = 1 if s[0] == '1' else 0
+  if len(nums) - 2 < 2 * k - (a^1):
     print(len(s))
   else:
-    max_sum = nums[a]
-    for i in range(0 - a, len(nums) - 2 * k, 2):
-      sum = 0
-      for j in range(i, i + 2 * k + 1):
-        if j == -1:
-          continue
-        sum += nums[j]
-      max_sum = max(max_sum, sum)
+    max_sum = 0
+    for i in range(a, 2 * k + a + 1):
+      max_sum += nums[i]
+    prev_sum = max_sum
+    for i in range(a, len(nums) - a - 2 * k - 2, 2):
+      cur_sum = prev_sum - nums[i] - nums[i + 1] + nums[2 * k + i + 1] + nums[2 * k + i + 2]
+      max_sum = max(cur_sum, max_sum)
+      prev_sum = cur_sum
+  
     print(max_sum)
       
