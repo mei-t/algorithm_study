6dac7c690969fda72971b9539f04ef416bd009d1
mei-t <me.7tspr@gmail.com>
2021-03-13 17:51:15 +0900

    Fix

--- AtCoder/cake_abc.py
+++ AtCoder/cake_abc.py
@@ -6,16 +6,15 @@ if __name__ == '__main__':
   B = sorted(list(map(int, input().split())), reverse=True)
   C = sorted(list(map(int, input().split())), reverse=True)
 
-  AB = []
-  for i in range(len(A)):
-    for j in range(len(B)):
-      AB.append(A[i] + B[j])
-  AB.sort(reverse=True)
-
   ABC = []
-  for i in range(min(len(AB), k)):
-    for j in range(len(C)):
-      ABC.append(AB[i] + C[j])
+  for a in range(len(A)):
+    for b in range(len(B)):
+      if (a + 1) * (b + 1) > k:
+        break
+      for c in range(len(C)):
+        if (a + 1) * (b + 1) * (c + 1) > k:
+          break
+        ABC.append(A[a] + B[b] + C[c])
   ABC.sort(reverse=True)
   for i in range(k):
     print(ABC[i])
