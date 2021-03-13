77a2bb2bdba4c6dce5b63eda19d98bd828c5fb15
mei-t <me.7tspr@gmail.com>
2021-03-13 16:42:22 +0900

    Add another solution

--- AtCoder/handstand.py
+++ AtCoder/handstand.py
@@ -1,20 +1,4 @@
-if __name__ == '__main__':
-  n, k = map(int, input().split())
-  s = input()
-  nums = []
-  prev = '1'
-  count = 0
-  for i in range(len(s)):
-    if s[i] == prev:
-      count += 1
-    else:
-      nums.append(count)
-      count = 1
-      prev = s[i]
-  nums.append(count)
-  if len(nums) % 2 == 0:
-    nums.append(0)
-
+def two_pointers(nums, k):
   add = 2 * k + 1
   left = 0
   right = 0
@@ -33,5 +17,39 @@ if __name__ == '__main__':
       right += 1
     max_sum = max(max_sum, cur_sum)
     
-  print(max_sum)
+  return max_sum
+
+def cumulative_sum(nums, k):
+    sums = [0] * (len(nums) + 1)
+    for i in range(len(nums)):
+      sums[i + 1] = sums[i] + nums[i]
+    
+    add = 2 * k + 1
+    max_sum = nums[0]
+    for i in range(0, len(nums), 2):
+      left = i
+      right = min(i + add, len(nums))
+      cur_sum = sums[right] - sums[left]
+      max_sum = max(max_sum, cur_sum)
+    return max_sum
+
+if __name__ == '__main__':
+  n, k = map(int, input().split())
+  s = input()
+  nums = []
+  prev = '1'
+  count = 0
+  for i in range(len(s)):
+    if s[i] == prev:
+      count += 1
+    else:
+      nums.append(count)
+      count = 1
+      prev = s[i]
+  nums.append(count)
+  if len(nums) % 2 == 0:
+    nums.append(0)
+
+#   print(two_pointers(nums, k))
+  print(cumulative_sum(nums, k))
       
