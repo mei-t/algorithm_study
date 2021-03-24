a92e02e038ff85ee0bb39f09ed365cd2f208eafb
mei-t <me.7tspr@gmail.com>
2021-03-24 18:13:06 +0900

    Add another solution

--- cracking_the_coding_interview/4-9.py
+++ cracking_the_coding_interview/4-9.py
@@ -30,6 +30,44 @@ def bst_list(root):
     create_list(deque([root]), [], res)
     return res
 
+def bst_list2(root):
+    res = []
+    if not root:
+        res.append([])
+        return res
+    
+    prefix = [root.val]
+    left_seq = bst_list2(root.left)
+    right_seq = bst_list2(root.right)
+
+    for left in left_seq:
+        for right in right_seq:
+            weaved = []
+            weave_list(left, right, weaved, prefix)
+            res.extend(weaved)
+    
+    return res
+
+def weave_list(left, right, weaved, prefix):
+    if len(left) == 0 or len(right) == 0:
+        res = copy(prefix)
+        res.extend(left)
+        res.extend(right)
+        weaved.append(res)
+        return
+    
+    head_left = left.pop(0)
+    prefix.append(head_left)
+    weave_list(left, right, weaved, prefix)
+    prefix.pop()
+    left.insert(0, head_left)
+
+    head_right = right.pop(0)
+    prefix.append(head_right)
+    weave_list(left, right, weaved, prefix)
+    prefix.pop()
+    left.insert(0, head_left)
+
 if __name__ == '__main__':
     node1 = TreeNode(2)
     node2 = TreeNode(1)
@@ -37,5 +75,5 @@ if __name__ == '__main__':
     root = node1
     node1.left = node2
     node1.right = node3
-    res = bst_list(root)
+    res = bst_list2(root)
     print(res)
