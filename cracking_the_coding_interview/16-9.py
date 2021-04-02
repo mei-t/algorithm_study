5382b2f1118d9b912b50b90451985b472d7131e1
mei-t <me.7tspr@gmail.com>
2021-04-02 18:03:32 +0900

    Add test

--- cracking_the_coding_interview/16-9.py
+++ cracking_the_coding_interview/16-9.py
@@ -1,3 +1,5 @@
+import unittest
+
 def negate(a):
     new_sign = -1 if a > 0 else 1
     ans = 0
@@ -14,5 +16,13 @@ def negate(a):
 def minus(a, b):
     return a + negate(b)
 
+class Test(unittest.TestCase):
+    def test_negate(self):
+        self.assertEqual(negate(10), -10)
+        self.assertEqual(negate(-10), 10)
+        self.assertEqual(negate(0), 0)
+        self.assertEqual(negate(274738), -274738)
+        self.assertEqual(negate(-274738), 274738)
+
 if __name__ == '__main__':
-    print(negate(-10))
+    unittest.main()
