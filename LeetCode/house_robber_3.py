40acaa7bd3e7ba4c40242a0ad02e6d8b62c3d791
mei-t <me.7tspr@gmail.com>
2021-04-05 12:15:17 +0900

    Add another solution.

--- LeetCode/house_robber_3.py
+++ LeetCode/house_robber_3.py
@@ -3,8 +3,21 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
-        
+
 class Solution:
+    def rob(self, root):
+        def check(node):
+            if not node:
+                return (0, 0)
+            left = check(node.left)
+            right = check(node.right)
+            
+            rob_cur = node.val + left[1] + right[1]
+            not_rob_cur = max(left) + max(right)
+            return (rob_cur, not_rob_cur)
+        return max(check(root))
+
+class Solution1:
     def rob(self, root):
         return self.check(root, True, dict())
     
