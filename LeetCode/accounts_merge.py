794dde93755b89847d1111a15bc853cc69a322e5
mei-t <me.7tspr@gmail.com>
2021-04-05 18:02:47 +0900

    Add another solution

--- LeetCode/accounts_merge.py
+++ LeetCode/accounts_merge.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 # 59:51
 class AccountNode:
     def __init__(self, name, addrList, next):
@@ -38,3 +40,37 @@ class Solution:
         for nextNode in accountNode.next:
             if nextNode not in visited:
                 self.depthFirstSearch(nextNode, visited, addrSet)
+
+
+class DSU:
+    def __init__(self):
+        self.par = list(range(10001))
+    
+    def find(self, x):
+        if self.par[x] != x:
+            self.par[x] = self.find(self.par[x])
+        return self.par[x]
+    
+    def union(self, x, y):
+        self.par[self.find(x)] = self.find(y)
+
+class Solution2:
+    def accountsMerge(self, accounts):
+        em_to_name = dict()
+        em_to_id = dict()
+        index = 0
+        dsu = DSU()
+        for account in accounts:
+            name = account[0]
+            for email in account[1:]:
+                em_to_name[email] = name
+                if email not in em_to_id:
+                    em_to_id[email] = index
+                    index += 1
+                dsu.union(em_to_id[account[1]], em_to_id[email])
+                
+        ans = defaultdict(list)
+        for email in em_to_name:
+            ans[dsu.find(em_to_id[email])].append(email)
+        
+        return [[em_to_name[v[0]]] + sorted(v) for v in ans.values()]
