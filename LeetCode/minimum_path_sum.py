b47277184c95cc34dfc34eb6cf45d217928fe820
mei-t <me.7tspr@gmail.com>
2021-04-05 14:33:07 +0900

    Add better solution

--- LeetCode/minimum_path_sum.py
+++ LeetCode/minimum_path_sum.py
@@ -1,5 +1,5 @@
 # 14:30
-class Solution:
+class Solution1:
     def minPathSum(self, grid):
         dp = [[0] * len(grid[0]) for _ in range(len(grid))]
         
@@ -13,3 +13,19 @@ class Solution:
             for j in range(1, len(grid[0])):
                 dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
         return dp[-1][-1]
+
+# 若干消化不良
+class Solution:
+    def minPathSum(self, grid):
+        dp = [0] * len(grid[0])
+        for i in reversed(range(len(grid))):
+            for j in reversed(range(len(grid[0]))):
+                if i == len(grid) - 1 and j != len(grid[0]) - 1:
+                    dp[j] = grid[i][j] + dp[j + 1]
+                elif j == len(grid[0]) - 1 and i != len(grid) - 1:
+                    dp[j] = grid[i][j] + dp[j]
+                elif j != len(grid[0]) - 1 and i != len(grid) - 1:
+                    dp[j] = grid[i][j] + min(dp[j], dp[j + 1])
+                else:
+                    dp[j] = grid[i][j]
+        return dp[0]
