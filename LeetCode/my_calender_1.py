357e0fc3c120a4986728b02ddb49a71439ac47ac
mei-t <me.7tspr@gmail.com>
2021-04-06 12:41:27 +0900

    Add another solution

--- LeetCode/my_calender_1.py
+++ LeetCode/my_calender_1.py
@@ -1,4 +1,36 @@
+class TreeNode:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        self.left = None
+        self.right = None
+    
+    def insert(self, node):
+        if node.start >= self.end:
+            if not self.right:
+                self.right = node
+                return True
+            return self.right.insert(node)
+        if node.end <= self.start:
+            if not self.left:
+                self.left = node
+                return True
+            return self.left.insert(node)
+        return False
+
 class MyCalendar:
+
+    def __init__(self):
+        self.root = None
+
+    def book(self, start, end):
+        node = TreeNode(start, end)
+        if not self.root:
+            self.root = node
+            return True
+        return self.root.insert(node)
+
+class MyCalendar2:
 
     def __init__(self):
         self.calender = []
