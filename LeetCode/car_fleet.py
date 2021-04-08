f8cb37459ec0458505a8d281de402965915f41f6
mei-t <me.7tspr@gmail.com>
2021-04-08 12:32:24 +0900

    Add car_fleet.py.

---
+++ LeetCode/car_fleet.py
@@ -0,0 +1,18 @@
+class Solution:
+    def carFleet(self, target, position, speed):
+        if not position:
+            return 0
+        position = [target - position[i] for i in range(len(position))]
+        cars = list(zip(position, speed))
+        cars.sort()
+        
+        count = 1
+        head_time = cars[0][0] / cars[0][1]
+        cur_i = 1
+        while cur_i < len(cars):
+            cur_time = cars[cur_i][0] / cars[cur_i][1]
+            if head_time < cur_time:
+                head_time = cur_time
+                count += 1
+            cur_i += 1
+        return count
