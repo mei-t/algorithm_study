aa47037b843fe3632d4ea0fe460e4929f556d1e0
mei-t <me.7tspr@gmail.com>
2021-04-18 10:28:09 +0900

    Add another solution

--- LeetCode/3_sum_closest.py
+++ LeetCode/3_sum_closest.py
@@ -1,3 +1,4 @@
+# O(n^2)
 class Solution:
     def threeSumClosest(self, nums, target):
         nums.sort()
@@ -15,5 +16,28 @@ class Solution:
                     l += 1
                 else:
                     h -= 1
+        
+        return diff + target
+
+# O((n^2)logn)
+class Solution2:
+    def threeSumClosest(self, nums, target):
+        nums.sort()
+        diff = float('inf')
+        for i in range(0, len(nums) - 2):
+            for j in range(i + 1, len(nums) - 1):
+                l = j + 1
+                r = len(nums)
+                while l < r:
+                    mid = (l + r) // 2
+                    tmp = nums[i] + nums[j] + nums[mid]
+                    if abs(tmp - target) < abs(diff):
+                        diff = tmp - target
+                    if tmp - target == 0:
+                        break
+                    if tmp - target < 0:
+                        l = mid + 1
+                    else:
+                        r = mid
         
         return diff + target
