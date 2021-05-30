d2377b16c4ca727084e67e44a740ed37ae145607
mei-t <me.7tspr@gmail.com>
2021-05-30 15:53:25 +0900

    Add another solution.

--- LeetCode/merge_two_sorted_lists.py
+++ LeetCode/merge_two_sorted_lists.py
@@ -54,4 +54,18 @@ class Solution2:
                 l2 = l2.next
             prev = prev.next
         return prehead.next
+
+# TC: O(m + n)
+# SC: O(m + n)
+class Solution3:
+    def mergeTwoLists(self, l1, l2):
+        if not l2:
+            return l1
+        if not l1:
+            return l2
+        if l1.val <= l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        l2.next = self.mergeTwoLists(l1, l2.next)
+        return l2
         
